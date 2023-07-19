678e2f874bb380612ca0b3b6614cb90d17202885
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-07-19 13:09:47 +0200

    Remoduled code in two helper classes

--- eudi_wallet_python/tools/jwt.py
+++ eudi_wallet_python/tools/jwt.py
@@ -22,10 +22,27 @@ DEFAULT_JWS_ALG = "RS256"
 DEFAULT_JWE_ALG = "RSA-OAEP"
 DEFAULT_JWE_ENC = "A256CBC-HS512"
 
+def unpad_jwt_header(jwt: str) -> dict:
+    b = jwt.split(".")[0]
+    padded = f"{b}{'=' * divmod(len(b), 4)[1]}"
+    data = json.loads(base64.urlsafe_b64decode(padded))
+    return data
 
-class JWE():
-    def __init__(self, plain_dict: Union[dict, str, int, None], jwk: JWK, **kwargs):
-        _key = key_from_jwk_dict(jwk.as_dict())
+def verify_at_hash(id_token, access_token) -> bool:
+    id_token_at_hash = id_token['at_hash']
+    at_hash = left_hash(access_token, "HS256")
+    if at_hash != id_token_at_hash:
+        raise Exception(
+            f"at_hash error: {at_hash} != {id_token_at_hash}"
+        )
+    return True
+
+class JWEHelper():
+    def __init__(self, jwk: JWK):
+        self.jwk = jwk
+    
+    def encrypt(self, plain_dict: Union[dict, str, int, None], **kwargs) -> str:
+        _key = key_from_jwk_dict(self.jwk.as_dict())
 
         if isinstance(_key, cryptojwt.jwk.rsa.RSAKey):
             JWE_CLASS = JWE_RSA
@@ -51,40 +68,35 @@ class JWE():
             **kwargs
         )
 
-        self.jwe = _keyobj.encrypt(_key.public_key())
+        return _keyobj.encrypt(_key.public_key())
+        
+    def decrypt(self, jwe: str) -> dict:
+        try:
+            jwe_header = unpad_jwt_header(jwe)
+        except (binascii.Error, Exception) as e:
+            raise VerificationError("The JWT is not valid")
 
+        _alg = jwe_header.get("alg", DEFAULT_JWE_ALG)
+        _enc = jwe_header.get("enc", DEFAULT_JWE_ENC)
+        jwe_header.get("kid")
 
-def unpad_jwt_header(jwt: str) -> dict:
-    b = jwt.split(".")[0]
-    padded = f"{b}{'=' * divmod(len(b), 4)[1]}"
-    data = json.loads(base64.urlsafe_b64decode(padded))
-    return data
+        _decryptor = factory(jwe, alg=_alg, enc=_enc)
 
+        _dkey = key_from_jwk_dict(self.jwk.as_dict())
+        msg = _decryptor.decrypt(jwe, [_dkey])
 
-def decrypt_jwe(jwe: str, jwk_dict: dict) -> dict:
-    try:
-        jwe_header = unpad_jwt_header(jwe)
-    except (binascii.Error, Exception) as e:
-        raise VerificationError("The JWT is not valid")
+        try:
+            msg_dict = json.loads(msg)
+        except json.decoder.JSONDecodeError:
+            msg_dict = msg
+        return msg_dict
 
-    _alg = jwe_header.get("alg", DEFAULT_JWE_ALG)
-    _enc = jwe_header.get("enc", DEFAULT_JWE_ENC)
-    jwe_header.get("kid")
-
-    _decryptor = factory(jwe, alg=_alg, enc=_enc)
-
-    _dkey = key_from_jwk_dict(jwk_dict)
-    msg = _decryptor.decrypt(jwe, [_dkey])
-
-    try:
-        msg_dict = json.loads(msg)
-    except json.decoder.JSONDecodeError:
-        msg_dict = msg
-    return msg_dict
-
-class JWS():
-    def __init__(self, jwk: JWK, plain_dict: Union[dict, str, int, None], alg: str = "RS256", protected: dict = {}, **kwargs):
-        _key = key_from_jwk_dict(jwk.as_dict())
+class JWSHelper():
+    def __init__(self, jwk: JWK):
+        self.jwk = jwk
+        
+    def sign(self, plain_dict: Union[dict, str, int, None], alg: str = "RS256", protected: dict = {}, **kwargs) -> str:
+        _key = key_from_jwk_dict(self.jwk.as_dict())
         
         _payload: str | int | bytes = ""
             
@@ -99,29 +111,19 @@ class JWS():
         
         _signer = JWSec(_payload, alg=alg, **kwargs)
 
-        self.signature = _signer.sign_compact([_key], protected=protected, **kwargs)
-        
-def verify_jws(jws: JWS, pub_jwk: dict, **kwargs) -> str:
-    _key = key_from_jwk_dict(pub_jwk)
+        return _signer.sign_compact([_key], protected=protected, **kwargs)
+    
+    def verify(self, jws: str, **kwargs):
+        _key = key_from_jwk_dict(self.jwk.as_dict())
 
-    _head = unpad_jwt_header(jws.signature)
-    if _head.get("kid") != pub_jwk["kid"]:  # pragma: no cover
-        raise Exception(
-            f"kid error: {_head.get('kid')} != {pub_jwk['kid']}"
-        )
+        _head = unpad_jwt_header(jws)
+        if _head.get("kid") != self.jwk.as_dict()["kid"]:  # pragma: no cover
+            raise Exception(
+                f"kid error: {_head.get('kid')} != {self.jwk.as_dict()['kid']}"
+            )
 
-    _alg = _head["alg"]
+        _alg = _head["alg"]
 
-    verifier = JWSec(alg=_head["alg"], **kwargs)
-    msg = verifier.verify_compact(jws, [_key])
-    return msg
-
-
-def verify_at_hash(id_token, access_token) -> bool:
-    id_token_at_hash = id_token['at_hash']
-    at_hash = left_hash(access_token, "HS256")
-    if at_hash != id_token_at_hash:
-        raise Exception(
-            f"at_hash error: {at_hash} != {id_token_at_hash}"
-        )
-    return True
+        verifier = JWSec(alg=_head["alg"], **kwargs)
+        msg = verifier.verify_compact(jws, [_key])
+        return msg
