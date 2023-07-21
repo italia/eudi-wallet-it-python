787e5dc1748c4f03204fefd2f18785ecddbcec29
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-07-21 13:47:06 +0200

    Removed unecessary directory

--- wordpress/wp-content/themes/italiawp2/archive.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-get_header();
-
-get_template_part( 'template-parts/archive-loop' );
-
-get_sidebar();
-get_footer();
--- wordpress/wp-content/themes/italiawp2/single-circolari.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-get_header();
-
-if(!is_front_page()) get_template_part( 'template-parts/circolare-loop' );
-
-get_sidebar();
-get_footer();
