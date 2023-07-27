eb982e56cfeb79a308b1120ca1e25ad7d7866037
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-07-27 17:49:01 +0200

    Aligned metadata

--- pyeudiw/tests/tools/test_sd_jwt.py
+++ pyeudiw/tests/tools/test_sd_jwt.py
@@ -21,23 +21,23 @@ def test_issue_sd_jwt():
         holder_jwk
     )
     
-def test_verify_sd_jwt():
+def test_verify_sd_jwt():    
     issuer_jwk = JWK()
     holder_jwk = JWK()
-    
+        
     user_claims_path = "./pyeudiw/tests/tools/specifications.yml"
     
     issued_jwt = issue_sd_jwt(
         user_claims_path, 
-        {"issuer": "http://test.com", "default_exp": 60, "specification_file": "./pyeudiw/tests/tools/specifications.yml", "no_randomness": True},
+        {"issuer": "http://test.com", "default_exp": 60, "specification_file": user_claims_path, "no_randomness": True},
         issuer_jwk,
         holder_jwk
     )
     
-    testcase = load_yaml_specification("./pyeudiw/tests/tools/specifications.yml")
+    testcase = load_yaml_specification(user_claims_path)
     
     adapted_keys = _adapt_keys(
-        {"issuer": "http://test.com", "default_exp": 60, "specification_file": "./pyeudiw/tests/tools/specifications.yml", "no_randomness": True}, 
+        {"issuer": "http://test.com", "default_exp": 60, "specification_file": user_claims_path, "no_randomness": True}, 
         issuer_jwk, holder_jwk)
     
     sdjwt_at_holder = SDJWTHolder(
@@ -45,13 +45,11 @@ def test_verify_sd_jwt():
         serialization_format="compact",
     )
     sdjwt_at_holder.create_presentation(
-        {},
+        testcase,
         None,
         None,
         adapted_keys["holder_key"] if testcase.get("key_binding", False) else None,
     )
-    
-    print(sdjwt_at_holder.sd_jwt_presentation)
         
     verified_payload = verify_sd_jwt(
         sdjwt_at_holder.sd_jwt_presentation, 
@@ -59,7 +57,7 @@ def test_verify_sd_jwt():
             "issuer": "http://test.com", 
             "verifier": "http://test.com",
             "default_exp": 60, 
-            "specification_file": "./pyeudiw/tests/tools/specifications.yml", 
+            "specification_file": user_claims_path, 
             "no_randomness": True,
             "key_binding_nonce": ""
-        }, issuer_jwk, holder_jwk)    
+        }, issuer_jwk, holder_jwk)
