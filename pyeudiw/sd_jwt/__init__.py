924652d48bb3e7fc4524237305021a8d26a1c7d8
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-07-28 11:54:17 +0200

    feat: removed yml specification load from file

--- pyeudiw/sd_jwt/__init__.py
+++ pyeudiw/sd_jwt/__init__.py
@@ -3,7 +3,7 @@ from sd_jwt.verifier import SDJWTVerifier
 
 from sd_jwt.utils.demo_utils import get_jwk
 from sd_jwt.utils.formatting import textwrap_json
-from sd_jwt.utils.yaml_specification import load_yaml_specification, _yaml_load_specification
+from sd_jwt.utils.yaml_specification import _yaml_load_specification
 
 from pyeudiw.tools.utils import iat_now
 from pyeudiw.jwk import JWK
@@ -21,22 +21,20 @@ def _adapt_keys(settings: dict, issuer_key: JWK, holder_key: JWK, kty: str = "EC
 def load_specification_from_yaml_string(yaml_specification: str):
     return _yaml_load_specification(yaml_specification)
 
-def issue_sd_jwt(user_claims_path: str, settings: dict, issuer_key: JWK, holder_key: JWK) -> str:
-    user_claims = load_yaml_specification(user_claims_path)
-    
+def issue_sd_jwt(user_claims_path: str, specification: dict, settings: dict, issuer_key: JWK, holder_key: JWK) -> str:    
     claims = {
         "iss": settings["issuer"],
         "iat": iat_now(),
         "exp": iat_now() + (settings["default_exp"] * 60)  # in seconds
     }
 
-    user_claims.update(claims)
-    use_decoys = user_claims.get("add_decoy_claims", False)
+    specification.update(claims)
+    use_decoys = specification.get("add_decoy_claims", False)
     adapted_keys = _adapt_keys(settings, issuer_key, holder_key)
     
     SDJWTIssuer.unsafe_randomness = settings["no_randomness"]
     sdjwt_at_issuer = SDJWTIssuer(
-        user_claims,
+        specification,
         adapted_keys["issuer_key"],
         adapted_keys["holder_key"],
         add_decoy_claims=use_decoys,
@@ -44,7 +42,7 @@ def issue_sd_jwt(user_claims_path: str, settings: dict, issuer_key: JWK, holder_
         
     return {"jws": textwrap_json(sdjwt_at_issuer.serialized_sd_jwt), "issuance": sdjwt_at_issuer.sd_jwt_issuance}
 
-def verify_sd_jwt(sd_jwt_presentation: str, settings: dict, issuer_key: JWK, holder_key: JWK):
+def verify_sd_jwt(sd_jwt_presentation: str, specification: dict, settings: dict, issuer_key: JWK, holder_key: JWK):
     
     adapted_keys = _adapt_keys(settings, issuer_key, holder_key)
     
@@ -54,7 +52,6 @@ def verify_sd_jwt(sd_jwt_presentation: str, settings: dict, issuer_key: JWK, hol
         else:
             raise Exception(f"Unknown issuer: {issuer}")
         
-    specification = load_yaml_specification(settings["specification_file"])
     use_decoys = specification.get("add_decoy_claims", False)
     serialization_format = specification.get("serialization_format", "compact")
     
