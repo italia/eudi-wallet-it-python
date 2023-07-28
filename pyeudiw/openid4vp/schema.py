94cca15171480d36fcd5f7675f2e681872ac0714
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-07-28 15:10:23 +0200

    feat: Added jwt checks

--- pyeudiw/openid4vp/schema.py
+++ pyeudiw/openid4vp/schema.py
@@ -1,5 +1,25 @@
+import re
 from pydantic import BaseModel
+from typing_extensions import Annotated
+from pydantic.functional_validators import AfterValidator
 
+JWT_REGEX = "(^[\\w-]*\\.[\\w-]*\\.[\\w-]*[~]*$)"
+
+def checkJWT(jwt: str) -> str:
+    res = re.match(JWT_REGEX, jwt)
+    
+    assert res, f"Vp_token is not a jwt {jwt}"
+    
+    return jwt
+    
+def checkJWTList(jwt_list: list[str]) -> list[str]:
+    assert len(jwt_list) != 0, f"Vp_token is an empty list"
+    
+    for jwt in jwt_list:
+        checkJWT(jwt)
+    
+    return jwt_list
+        
 class DescriptorSchema(BaseModel):
     id: str
     path: str
@@ -12,6 +32,5 @@ class PresentationSubmissionSchema(BaseModel):
 
 class ResponseSchema(BaseModel):
     state: str
-    vp_token: str | list[str]
+    vp_token: Annotated[str, AfterValidator(checkJWT)] | Annotated[list[str], AfterValidator(checkJWTList)]
     presentation_submission: PresentationSubmissionSchema
-    
