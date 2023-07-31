34ef6d98c9c690bc1526dbc09d557a9f34674602
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-07-31 17:06:39 +0200

    fix: implemented logger critical errors

--- pyeudiw/storage/db_engine.py
+++ pyeudiw/storage/db_engine.py
@@ -1,7 +1,10 @@
 import uuid
+import logging
 import importlib
 from pyeudiw.storage.base_cache import BaseCache
 from pyeudiw.storage.base_storage import BaseStorage
+
+logger = logging.getLogger("openid4vp_backend")
 
 class DBEngine():
     def __init__(self, config: dict):
@@ -12,10 +15,10 @@ class DBEngine():
             storage_instance, cache_instance = self._handle_instance(db_conf)
             
             if storage_instance:
-                self.storages.append(storage_instance)
+                self.storages.append((db_name, storage_instance))
                 
             if cache_instance:
-                self.cache.append(cache_instance)
+                self.cache.append((db_name, cache_instance))
     
     def _handle_instance(instance: dict) -> dict[BaseStorage | None, BaseCache | None]:
         cache_conf = instance.get("cache", None)
@@ -39,15 +42,24 @@ class DBEngine():
     
     def init_session(self, dpop_proof: dict, attestation: dict):
         document_id = str(uuid.uuid4())
-        for storage in self.storages:
-            storage.init_session(dpop_proof, attestation)
+        for db_name, storage in self.storages:
+            try:
+                storage.init_session(dpop_proof, attestation)
+            except Exception as e:
+                logger.critical("Cannot write document with id {document_id} on {db_name}")
             
         return document_id
 
-    def update_request_object(self, document_id: str, request_object: dict):
-        for storage in self.storages:
-            storage.update_request_object(document_id, request_object)
+    def update_request_object(self, document_id: str, nonce: str, state: str, request_object: dict):
+        for db_name, storage in self.storages:
+            try:
+                storage.update_request_object(document_id, nonce, state, request_object)
+            except Exception as e:
+                logger.critical("Cannot update document with id {document_id} on {db_name}")
 
     def update_response_object(self, nonce: str, state: str, response_object: dict):
-        for storage in self.storages:
-            storage.update_response_object(nonce, state, response_object)
+        for db_name, storage in self.storages:
+            try:
+                storage.update_response_object(nonce, state, response_object)
+            except Exception as e:
+                logger.critical("Cannot update document with nonce {nonce} and state {state} on {db_name}")
