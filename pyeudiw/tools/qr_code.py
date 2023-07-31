da75f0f608885c5207a736e1f6aacc1f5f213eee
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2023-07-31 15:28:43 +0200

    refactor: from qrcode to pyqrcode (#62)
    * refactor: switch from qrcode to pyqrcode
    
    * fix: qrcode settings
    
    * fix: remove sample data from library examples
    
    ---------
    
    Co-authored-by: Giuseppe De Marco <giuseppe.demarco@teamdigitale.governo.it>

--- pyeudiw/tools/qr_code.py
+++ pyeudiw/tools/qr_code.py
@@ -1,65 +1,49 @@
-import qrcode
-import qrcode.image.svg
-import zlib
+import base64
+import io
 
-from io import BytesIO
+import pyqrcode
 
 
 class QRCode:
-    def __init__(self, data: str, size: int, color: str, logo_path: str, use_zlib: bool, logo_size_factor: int = 5) -> None:
-        compressed_request_data = None
+    def __init__(self, data: str, size: int, color: str):
+        """
+        Create a QR code from the given data
+        :param data: The data to be encoded
+        :param size: The size of the QR code. Maps to scale in pyqrcode
+        :param color: The color of the QR code
+        """
+        self.data = data
+        self.size = size
         self.color = color
 
-        if use_zlib:
-            compressed_request_data = zlib.compress(data.encode(), 9)
+        qr = pyqrcode.create(data)
+        # Copy the svg data to a string and close the buffer to avoid memory leaks
+        buffer = io.BytesIO()
+        qr.svg(buffer, scale=size, background="white", module_color=color)
+        self.svg = buffer.getvalue().decode("utf-8")
+        buffer.close()
 
-        qr_code = qrcode.QRCode(
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=size,
-        )
+    def to_svg(self) -> str:
+        """
+        Returns the svg data for the QR code as a string
+        :return: The svg data for the QR code
+        :rtype: str
+        """
+        return self.svg
 
-        qr_code.add_data(compressed_request_data or data)
+    def to_base64(self) -> str:
+        """
+        Returns the svg data for html
+        :return: The svg data for html, base64 encoded
+        :rtype: str
+        """
+        return base64.b64encode(self.svg.encode()).decode('utf-8')
 
-        # it must be SVG
-        qr_code.make()
-        qr_code.image_factory = qrcode.image.svg.SvgImage
-
-        self.qr_code = qr_code
-
-        self.qr_code_img = qr_code.make_image(
-            fill_color=color,
-            back_color="white"
-        )
-
-        # Add logo if present
-        if logo_path:
-            # TODO - svg doesn't have a size so we need to find another way
-            pass
-            # logo = Image.open(logo_path)
-            # wpercent = (size / float(logo.size[0]))
-            # hsize = int((float(logo.size[1]) * float(wpercent)))
-            # logo = logo.resize(
-            # (size * logo_size_factor, hsize * logo_size_factor), Image.LANCZOS)
-
-            # pos = ((self.qr_code_img.size[0] - logo.size[0]) //
-            # 2, (self.qr_code_img.size[1] - logo.size[1]) // 2)
-
-            # self.qr_code_img.paste(logo, pos)
-
-    def save_as_file(self, path) -> str:
-        self.qr_code_img.save(path)
-        return path
-
-    def as_svg(self) -> BytesIO:
-        stream = BytesIO()
-        self.qr_code_img.save(stream)
-        stream.seek(0)
-        return stream
-
-    def for_html(self):
-        stream = self.as_svg()
-        data = stream.read().decode()
-        # data = data.replace(
-        # "<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n", ""
-        # )
-        return data
+    def to_html(self) -> str:
+        """
+        Returns the svg data in a html img tag, encoded as base64
+        :return: Image tag with svg data for html
+        :rtype: str
+        """
+        b64 = self.to_base64()
+        return f'<img src="data:image/svg+xml;base64,{b64}">'
