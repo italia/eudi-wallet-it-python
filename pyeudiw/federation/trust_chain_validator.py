4434d105a0d4e0ec55b900b2f4c902d2924c6c94
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-08-02 12:08:21 +0200

    feat: implemented property is_valid

--- pyeudiw/federation/trust_chain_validator.py
+++ pyeudiw/federation/trust_chain_validator.py
@@ -1,18 +1,18 @@
-from pyeudiw.federation.statements import (
-    get_entity_configurations,
-    EntityStatement,
-)
-from pyeudiw.jwt.utils import unpad_jwt_payload
-
+import logging
+#from pyeudiw.federation.statements import (
+#    get_entity_configurations,
+#    EntityStatement,
+#)
+from pyeudiw.tools.utils import iat_now
+from pyeudiw.jwt.utils import unpad_jwt_payload, unpad_jwt_header
 
 logger = logging.getLogger("pyeudiw.federation")
-
 
 class StaticTrustChainValidator:
     def __init__(
         self,
         static_trust_chain :list,
-        trust_anchor_jwks :list
+        trust_anchor_jwks :list,
         **kwargs,
     ) -> None:
         
@@ -21,6 +21,22 @@ class StaticTrustChainValidator:
         self.trust_anchor_jwks = trust_anchor_jwks
         for k,v in kwargs.items():
             setattr(self, k, v)
+    
+    def _validate_exp(self, exp):
+        if exp < iat_now():
+            raise Exception(f"TA expiried")
+        
+    def _validate_keys(self, fed_jwks, st_header):
+        current_kid = st_header["kid"]
+        
+        validation_kid = None
+        
+        for key in fed_jwks:
+            if key["kid"] == current_kid:
+                validation_kid = key
+        
+        if validation_kid == None:
+            raise Exception(f"Kid not in chain")                
     
     @property
     def is_valid(self):
@@ -36,17 +52,43 @@ class StaticTrustChainValidator:
         # inspect the entity statement kid header to know which 
         # TA's public key to use for the validation
         
-        # if valid: exps.append(this-exp)
+        es_payload = unpad_jwt_payload(rev_tc[0])
+        es_exp = es_payload["exp"]
         
-        fed_jwks = unpad_jwt_payload(rev_tc[0])['jwks']['keys']
+        # if valid: exps.append(this-exp)
+        self._validate_exp(es_exp)
+        
+        #current_kid = es_payload["jwks"]["keys"][0]["kid"]
+        
+        fed_jwks = es_payload["jwks"]["keys"]
+                
         # for st in rev_tc[1:]:
         # validate the entire chain taking in cascade using fed_jwks
         # if valid -> update fed_jwks with $st
+        
+        for st in rev_tc[1:]:
+            st_header = unpad_jwt_header(st)
+            st_payload = unpad_jwt_payload(st)
+            
+            try:
+                self._validate_keys(fed_jwks, st_header)
+            except Exception as e:
+                logger.warning(f"Warning: {e}")
+                return False
+            
+            try:
+                self._validate_exp(st_payload["exp"])
+            except Exception as e:
+                logger.warning(f"Warning: {e}")
+                return False
+            
+            fed_jwks = st_payload["jwks"]["keys"]
+            
+        return True
     
     def update(self):
-        
-        
         for st in self.static_trust_chain:
+            pass
             # if EC -> download again from well-known/openid-federation
             # if ES:
             #   if source_endpoint is available -> take it directly from it
