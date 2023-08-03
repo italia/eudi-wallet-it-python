41d4011b7b069acc5acb02c88bf77d52ab224a25
Salvatore Laiso <salvatorelaiso@gmail.com>
2023-08-03 17:11:50 +0200

    feat: add entity configuration validation with optional context

--- pyeudiw/openid4vp/schemas/entity_configuration.py
+++ pyeudiw/openid4vp/schemas/entity_configuration.py
@@ -1,16 +1,47 @@
 from typing import List, Literal
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, field_validator
+from pydantic_core.core_schema import FieldValidationInfo
 
 from pyeudiw.jwk.schema import JwksSchema
 from pyeudiw.openid4vp.schemas.federation_entity import FederationEntity
 from pyeudiw.openid4vp.schemas.wallet_relying_party import WalletRelyingParty
 
 
+_default_supported_algorithms = [
+    "RS256",
+    "RS384",
+    "RS512",
+    "ES256",
+    "ES384",
+    "ES512",
+    "PS256",
+    "PS384",
+    "PS512",
+]
+
+
+def _check_alg(alg: str, info: FieldValidationInfo):
+    if not info.context:
+        supported_algorithms = _default_supported_algorithms
+    else:
+        supported_algorithms = info.context.get(
+            "supported_algorithms", _default_supported_algorithms)
+
+    if alg not in supported_algorithms:
+        raise ValueError(f"Unsupported algorithm: {alg}.\n  "
+                         f"Supported algorithms: {supported_algorithms}.\n")
+
+
 class EntityConfigurationHeader(BaseModel):
     alg: str
     kid: str
     typ: Literal["entity-statement+jwt"]
+
+    @field_validator("alg")
+    @classmethod
+    def check_alg(cls, alg, info: FieldValidationInfo):
+        return _check_alg(alg, info)
 
 
 class EntityConfigurationMetadataSchema(BaseModel):
--- pyeudiw/openid4vp/schemas/wallet_relying_party.py
+++ pyeudiw/openid4vp/schemas/wallet_relying_party.py
@@ -1,8 +1,44 @@
 from typing import List, Dict, Any
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, field_validator
+from pydantic_core.core_schema import FieldValidationInfo
 
 from pyeudiw.jwk.schema import JwksSchema
+
+_default_algorithms = {
+    "authorization_signed_response_alg": [
+        "RS256",
+        "ES256"
+    ],
+    "authorization_encrypted_response_alg": [
+        "RSA-OAEP",
+        "RSA-OAEP-256",
+    ],
+    "authorization_encrypted_response_enc": [
+        "A128CBC-HS256",
+        "A192CBC-HS384",
+        "A256CBC-HS512",
+        "A128GCM",
+        "A192GCM",
+        "A256GCM",
+    ],
+    "id_token_signed_response_alg": [
+        "RS256",
+        "ES256"
+    ],
+    "id_token_encrypted_response_alg": [
+        "RSA-OAEP",
+        "RSA-OAEP-256",
+    ],
+    "id_token_encrypted_response_enc": [
+        "A128CBC-HS256",
+        "A192CBC-HS384",
+        "A256CBC-HS512",
+        "A128GCM",
+        "A192GCM",
+        "A256GCM",
+    ]
+}
 
 
 class WalletRelyingParty(BaseModel):
@@ -17,7 +53,6 @@ class WalletRelyingParty(BaseModel):
     vp_formats: Dict[str, Dict[str, List[str]]]
     presentation_definitions: List[Any]
     default_max_age: int
-    # TODO: check if the following lists can be narrowed down to a list of Literals
     authorization_signed_response_alg: List[str]
     authorization_encrypted_response_alg: List[str]
     authorization_encrypted_response_enc: List[str]
@@ -26,3 +61,34 @@ class WalletRelyingParty(BaseModel):
     id_token_signed_response_alg: List[str]
     id_token_encrypted_response_alg: List[str]
     id_token_encrypted_response_enc: List[str]
+
+    @classmethod
+    def _get_algorithms_supported(cls, name: str, info: FieldValidationInfo) -> list[str]:
+        if not info.context:
+            return _default_algorithms[name]
+        return info.context.get(name, _default_algorithms[name])
+
+    @classmethod
+    def _check_algorithms(cls, algorithms: list[str], name: str, info: FieldValidationInfo):
+        supported_algorithms = WalletRelyingParty._get_algorithms_supported(name, info)
+        for alg in algorithms:
+            if alg not in supported_algorithms:
+                raise ValueError(f"Unsupported algorithm: {alg} for {name}.\n  "
+                                 f"Supported algorithms: {supported_algorithms}.\n")
+        return algorithms
+
+    @field_validator(
+        "authorization_signed_response_alg",
+        "authorization_encrypted_response_alg",
+        "authorization_encrypted_response_enc",
+        "id_token_signed_response_alg",
+        "id_token_encrypted_response_alg",
+        "id_token_encrypted_response_enc"
+    )
+    @classmethod
+    def check_alg(cls, value, info: FieldValidationInfo):
+        return WalletRelyingParty._check_algorithms(
+            value,
+            info.field_name,
+            info
+        )
