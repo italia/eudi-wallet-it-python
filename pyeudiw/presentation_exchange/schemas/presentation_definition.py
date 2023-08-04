9ee4fe72f3c8b7bfe542ad32826a965d709611d1
Salvatore Laiso <salvatorelaiso@gmail.com>
2023-08-04 12:31:58 +0200

    fix: input descriptor format schema

--- pyeudiw/presentation_exchange/schemas/presentation_definition.py
+++ pyeudiw/presentation_exchange/schemas/presentation_definition.py
@@ -3,14 +3,25 @@ from typing import Any, Dict, List, Literal, Optional
 from pydantic import BaseModel
 
 
+class InputDescriptorJwt(BaseModel):
+    alg: List[str]
+
+
+class MsoMdoc(BaseModel):
+    alg: List[str]
+
+
+class FormatSchema(BaseModel):
+    jwt: Optional[InputDescriptorJwt] = None
+    mso_mdoc: Optional[MsoMdoc] = None
+    constraints: Optional[Dict[str, Any]] = None
+
+
 class InputDescriptor(BaseModel):
     id: str
     name: Optional[str] = None
     purpose: Optional[str] = None
-    format: Optional[str | dict[str, Any]] = None
-    jwt: Optional[Dict[Literal["alg"], List[str]]] = None
-    mso_mdoc: Optional[Dict[Literal["alg"], List[str]]] = None
-    constraints: Optional[Dict[str, Any]] = None
+    format: Optional[str | FormatSchema] = None
 
 
 class PresentationDefinition(BaseModel):
