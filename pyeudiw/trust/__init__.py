822b97752a366942e86edbf9b1e20f5121a70c8d
Pasquale De Rose <pasquale.de.rose@it.ey.com>
2023-08-09 16:46:10 +0200

    feat: added property entity_id

--- pyeudiw/trust/__init__.py
+++ pyeudiw/trust/__init__.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from pyeudiw.federation.trust_chain_validator import StaticTrustChainValidator
-from pyeudiw.storage.base_storage import BaseStorage
+from pyeudiw.storage.db_engine import DBEngine
 from pyeudiw.trust.exceptions import NoTrustChainProvided
 
 class TrustEvaluationHelper:
-    def __init__(self, entityid: str, storage: BaseStorage,  **kwargs):
-        self.entityid = entityid
+    def __init__(self, storage: DBEngine,  **kwargs):
         self.exp = None
         self.trust_chain = None
         self.storage = storage
+        self.entity_id = None
         
         for k, v in kwargs.items():
             setattr(self, k, v)
@@ -20,11 +20,13 @@ class TrustEvaluationHelper:
     
     def _handle_chain(self, trust_chain: list[str]):
         tc = StaticTrustChainValidator(trust_chain)
+        self.entity_id = tc.get_entityID()
+        
         _is_valid = tc.is_valid
         self.exp = tc.get_exp()
 
         if not _is_valid:
-            db_chain = self.storage.find_chain(tc.get_entityID())["federation"]["chain"]
+            db_chain = self.storage.find_chain(self.entity_id)["federation"]["chain"]
             
             if db_chain is not None and \
                 StaticTrustChainValidator(db_chain).is_valid:
@@ -35,9 +37,9 @@ class TrustEvaluationHelper:
             self.trust_chain = tc.get_chain()
             
             if db_chain is None:
-                self.storage.add_chain(tc.get_entityID(), tc.get_chain(), datetime.fromtimestamp(tc.get_exp()))
+                self.storage.add_chain(self.entity_id, tc.get_chain(), datetime.fromtimestamp(tc.get_exp()))
             else:
-                self.storage.update_chain(tc.get_entityID(), tc.get_chain(), datetime.fromtimestamp(tc.get_exp()))
+                self.storage.update_chain(self.entity_id, tc.get_chain(), datetime.fromtimestamp(tc.get_exp()))
 
         return _is_valid
     
@@ -51,3 +53,7 @@ class TrustEvaluationHelper:
     
     def x509(self):
         pass
+    
+    @property
+    def entity_id(self):
+        return self.entity_id
