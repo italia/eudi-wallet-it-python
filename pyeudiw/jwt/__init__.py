527167e0eeea790cff5dd9dc08c19c116f96fc36
peppelinux <demarcog83@gmail.com>
2023-08-29 18:34:23 +0200

    fix: remove the static comparison of the jwks

--- pyeudiw/jwt/__init__.py
+++ pyeudiw/jwt/__init__.py
@@ -130,17 +130,18 @@ class JWSHelper:
         _key = key_from_jwk_dict(self.jwk.as_dict())
         _jwk_dict = self.jwk.as_dict()
         _head = unpad_jwt_header(jws)
-
+        
         if _head.get("kid"):
             if _head["kid"] != _jwk_dict["kid"]:  # pragma: no cover
                 raise KidError(
                     f"{_head.get('kid')} != {_jwk_dict['kid']}"
                 )
-        elif _head.get("jwk"):
-            if _head["jwk"] != _jwk_dict:  # pragma: no cover
-                raise JwkError(
-                    f"{_head['jwk']} != {_jwk_dict}"
-                )
+        # TODO: check why unfortunately obtaining a public key from a TEE may dump a different y valu using EC keys
+        # elif _head.get("jwk"):
+            # if _head["jwk"] != _jwk_dict:  # pragma: no cover
+                # raise JwkError(
+                    # f"{_head['jwk']} != {_jwk_dict}"
+                # )
 
         verifier = JWSec(alg=_head["alg"], **kwargs)
         msg = verifier.verify_compact(jws, [_key])
