9d633f88518513ec4d0c2fa2f75d31d38104819f
root <root@it-wallet-tmp.mpokl4s14jhunpbwqmmurqxrkb.fx.internal.cloudapp.net>
2023-09-20 13:15:45 +0000

    feat: decode jwt str or bytes

--- pyeudiw/jwt/utils.py
+++ pyeudiw/jwt/utils.py
@@ -7,6 +7,8 @@ JWT_REGEXP = r'^[\w\-]+\.[\w\-]+\.[\w\-]+'
 
 
 def unpad_jwt_element(jwt: str, position: int) -> dict:
+    if isinstance(jwt, bytes):
+        jwt = jwt.decode()
     b = jwt.split(".")[position]
     padded = f"{b}{'=' * divmod(len(b), 4)[1]}"
     data = json.loads(base64.urlsafe_b64decode(padded))
