69dc23268e63c0498aa66f260fc3d15867496959
PascalDR <akira.zeta@gmail.com>
2023-11-07 17:47:58 +0100

    fix: fixed conversion for right handling

--- pyeudiw/x509/verify.py
+++ pyeudiw/x509/verify.py
@@ -1,5 +1,9 @@
+import logging
 from OpenSSL import crypto
 from datetime import datetime
+from ssl import DER_cert_to_PEM_cert
+
+logger = logging.getLogger(__name__)
 
 def verify_x509_cert_chain(x509: dict[str, list[bytes] | datetime]) -> bool:
     if datetime.now() > x509["exp"]:
@@ -8,15 +12,18 @@ def verify_x509_cert_chain(x509: dict[str, list[bytes] | datetime]) -> bool:
     try:
         store = crypto.X509Store()
 
-        cert_chain = x509["x5c"]
+        pems = [DER_cert_to_PEM_cert(cert) for cert in x509["x5c"]]
+        x509_certs = [crypto.load_certificate(crypto.FILETYPE_PEM, str(pem)) for pem in pems]
 
-        for cert in cert_chain[:-1]:
+        for cert in x509_certs[:-1]:
+            breakpoint()
             store.add_cert(cert)
 
-        store_ctx = crypto.X509StoreContext(store, cert_chain[-1])
+        store_ctx = crypto.X509StoreContext(store, x509_certs[-1])
 
         store_ctx.verify_certificate()
 
         return True
     except Exception as e:
+        logging.warn("x509 verification failed: {}".format(e))
         return False
