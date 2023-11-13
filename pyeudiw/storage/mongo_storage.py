ba640e7dcbe7e443ee48350b12b7ff10bd869124
Pasquale De Rose <akira.zeta@gmail.com>
2023-11-13 15:43:14 +0100

    Update pyeudiw/storage/mongo_storage.py
    Co-authored-by: Giuseppe De Marco <giuseppe.demarco@teamdigitale.governo.it>

--- pyeudiw/storage/mongo_storage.py
+++ pyeudiw/storage/mongo_storage.py
@@ -242,7 +242,7 @@ class MongoStorage(BaseStorage):
         trust_name = "x509" if trust_type == TrustType.X509 else "federation"
         attestation_name = "pem" if trust_type == TrustType.X509 else "entity_configuration"
 
-        trust_entity = ta.get(trust_name, None) or {}
+        trust_entity = ta.get(trust_name, {})
 
         trust_entity[attestation_name] = attestation
         trust_entity["exp"] = exp
