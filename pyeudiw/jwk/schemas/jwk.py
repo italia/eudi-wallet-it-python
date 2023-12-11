96abf606e13bc37b2363686292e327e21e6e676e
Salvatore Laiso <salvatore.laiso@it.ey.com>
2023-12-11 14:51:21 +0100

    feat: dynamic JWK schema loading

---
+++ pyeudiw/jwk/schemas/jwk.py
@@ -0,0 +1,24 @@
+from typing import Literal, Annotated, Union, Optional
+
+from pydantic import BaseModel, Field
+
+
+class JwkBaseModel(BaseModel):
+    use: Optional[Literal["sig", "enc"]] = None
+    kid: Optional[str] = None
+
+
+class RSAJwkSchema(JwkBaseModel):
+    kty: Literal["RSA"]
+    n: str
+    e: str
+
+
+class ECJwkSchema(JwkBaseModel):
+    kty: Literal["EC"]
+    crv: Literal["P-256", "P-384", "P-521"]
+    x: str
+    y: str
+
+
+JwkSchema = Annotated[Union[ECJwkSchema, RSAJwkSchema], Field(discriminator="kty")]
