a2518f1628a8a889f7de233c60731dcd3981b193
Salvatore Laiso <salvatore.laiso@it.ey.com>
2023-12-11 14:33:36 +0100

    test: integration test with EC keys

---
+++ example/satosa/integration_test/settings_ec.py
@@ -0,0 +1,135 @@
+
+from cryptojwt.jws.jws import JWS
+from cryptojwt.jwk.jwk import key_from_jwk_dict
+from pyeudiw.tests.federation.base_ec import (
+    NOW,
+    EXP,
+    leaf_wallet_jwk,
+    ta_ec,
+    ta_jwk
+)
+
+from pyeudiw.tools.utils import iat_now, exp_from_now
+
+
+RP_EID = "https://localhost:10000/OpenID4VP"
+
+CONFIG_DB = {
+    "mongo_db": {
+        "storage": {
+            "module": "pyeudiw.storage.mongo_storage",
+            "class": "MongoStorage",
+            "init_params": {
+                "url": "mongodb://localhost:27017/",
+                "conf": {
+                    "db_name": "eudiw",
+                    "db_sessions_collection": "sessions",
+                    "db_trust_attestations_collection": "trust_attestations",
+                    "db_trust_anchors_collection": "trust_anchors"
+                },
+                "connection_params": {}
+            }
+        }
+    }
+}
+
+
+WALLET_INSTANCE_ATTESTATION = {
+    "iss": "https://wallet-provider.example.org",
+    "sub": "vbeXJksM45xphtANnCiG6mCyuU4jfGNzopGuKvogg9c",
+    "type": "WalletInstanceAttestation",
+    "policy_uri": "https://wallet-provider.example.org/privacy_policy",
+    "tos_uri": "https://wallet-provider.example.org/info_policy",
+    "logo_uri": "https://wallet-provider.example.org/logo.svg",
+    "asc": "https://wallet-provider.example.org/LoA/basic",
+    "cnf":
+    {
+        "jwk": leaf_wallet_jwk.serialize()
+    },
+    "authorization_endpoint": "eudiw:",
+    "response_types_supported": [
+        "vp_token"
+    ],
+    "vp_formats_supported": {
+        "jwt_vp_json": {
+            "alg_values_supported": ["ES256"]
+        },
+        "jwt_vc_json": {
+            "alg_values_supported": ["ES256"]
+        }
+    },
+    "request_object_signing_alg_values_supported": [
+        "ES256"
+    ],
+    "presentation_definition_uri_supported": False,
+    "iat": iat_now(),
+    "exp": exp_from_now()
+}
+
+rp_jwks = [
+    {
+        "kty": "RSA",
+        "d": "QUZsh1NqvpueootsdSjFQz-BUvxwd3Qnzm5qNb-WeOsvt3rWMEv0Q8CZrla2tndHTJhwioo1U4NuQey7znijhZ177bUwPPxSW1r68dEnL2U74nKwwoYeeMdEXnUfZSPxzs7nY6b7vtyCoA-AjiVYFOlgKNAItspv1HxeyGCLhLYhKvS_YoTdAeLuegETU5D6K1xGQIuw0nS13Icjz79Y8jC10TX4FdZwdX-NmuIEDP5-s95V9DMENtVqJAVE3L-wO-NdDilyjyOmAbntgsCzYVGH9U3W_djh4t3qVFCv3r0S-DA2FD3THvlrFi655L0QHR3gu_Fbj3b9Ybtajpue_Q",
+        "e": "AQAB",
+        "kid": "9Cquk0X-fNPSdePQIgQcQZtD6J0IjIRrFigW2PPK_-w",
+        "n": "utqtxbs-jnK0cPsV7aRkkZKA9t4S-WSZa3nCZtYIKDpgLnR_qcpeF0diJZvKOqXmj2cXaKFUE-8uHKAHo7BL7T-Rj2x3vGESh7SG1pE0thDGlXj4yNsg0qNvCXtk703L2H3i1UXwx6nq1uFxD2EcOE4a6qDYBI16Zl71TUZktJwmOejoHl16CPWqDLGo9GUSk_MmHOV20m4wXWkB4qbvpWVY8H6b2a0rB1B1YPOs5ZLYarSYZgjDEg6DMtZ4NgiwZ-4N1aaLwyO-GLwt9Vf-NBKwoxeRyD3zWE2FXRFBbhKGksMrCGnFDsNl5JTlPjaM3kYyImE941ggcuc495m-Fw",
+        "p": "2zmGXIMCEHPphw778YjVTar1eycih6fFSJ4I4bl1iq167GqO0PjlOx6CZ1-OdBTVU7HfrYRiUK_BnGRdPDn-DQghwwkB79ZdHWL14wXnpB5y-boHz_LxvjsEqXtuQYcIkidOGaMG68XNT1nM4F9a8UKFr5hHYT5_UIQSwsxlRQ0",
+        "q": "2jMFt2iFrdaYabdXuB4QMboVjPvbLA-IVb6_0hSG_-EueGBvgcBxdFGIZaG6kqHqlB7qMsSzdptU0vn6IgmCZnX-Hlt6c5X7JB_q91PZMLTO01pbZ2Bk58GloalCHnw_mjPh0YPviH5jGoWM5RHyl_HDDMI-UeLkzP7ImxGizrM"
+    },
+    {
+        'kty': 'EC',
+        'kid': 'xPFTWxeGHTVTaDlzGad0MKN5JmWOSnRqEjJCtvQpoyg',
+        'crv': 'P-256',
+        'x': 'EkMoe7qPLGMydWO_evC3AXEeXJlLQk9tNRkYcpp7xHo',
+        'y': 'VLoHFl90D1SdTTjMvNf3WssWiCBXcU1lGNPbOmcCqdU',
+        'd': 'oGzjgBbIYNL9opdJ_rDPnCJF89yN8yj8wegdkYfaxw0'
+    }
+]
+rp_ec = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": RP_EID,
+    "sub": RP_EID,
+    'jwks': {"keys": rp_jwks},
+    "metadata": {
+        "wallet_relying_party": {
+            'jwks': {"keys": []}
+        },
+        "federation_entity": {
+            "organization_name": "OpenID Wallet Verifier example",
+            "homepage_uri": "https://verifier.example.org/home",
+            "policy_uri": "https://verifier.example.org/policy",
+            "logo_uri": "https://verifier.example.org/static/logo.svg",
+            "contacts": [
+                "tech@verifier.example.org"
+            ]
+        }
+    },
+    "authority_hints": [
+        ta_ec['iss']
+    ]
+}
+rp_signer = JWS(
+    rp_ec, alg="ES256",
+    typ="application/entity-statement+jwt"
+)
+
+
+_es = ta_es = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": ta_ec['iss'],
+    "sub": RP_EID,
+    'jwks': {
+        'keys': rp_jwks
+    }
+}
+ta_signer = JWS(
+    _es, alg="ES256",
+    typ="application/entity-statement+jwt"
+)
+
+its_trust_chain = [
+    rp_signer.sign_compact([key_from_jwk_dict(rp_jwks[1])]),
+    ta_signer.sign_compact([ta_jwk])
+]
---
+++ pyeudiw/tests/federation/base_ec.py
@@ -0,0 +1,218 @@
+from cryptojwt.jwk.ec import new_ec_key
+from cryptojwt.jws.jws import JWS
+
+import json
+import pyeudiw.federation.trust_chain_validator as tcv_test
+from pyeudiw.tools.utils import iat_now, exp_from_now
+
+httpc_params = {
+    "connection": {"ssl": True},
+    "session": {"timeout": 6},
+}
+
+NOW = iat_now()
+EXP = exp_from_now(5000)
+
+ec_crv = "P-256"
+ec_alg = "ES256"
+
+# Define intermediate ec
+intermediate_jwk = new_ec_key(ec_crv, alg=ec_alg)
+
+# Define TA ec
+ta_jwk = new_ec_key(ec_crv, alg=ec_alg)
+
+# Define leaf Credential Issuer
+leaf_cred_jwk = new_ec_key(ec_crv, alg=ec_alg)
+leaf_cred_jwk_prot = new_ec_key(ec_crv, alg=ec_alg)
+leaf_cred = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": "https://credential_issuer.example.org",
+    "sub": "https://credential_issuer.example.org",
+    'jwks': {"keys": []},
+    "metadata": {
+        "openid_credential_issuer": {
+            'jwks': {"keys": []}
+        },
+        "federation_entity": {
+            "organization_name": "OpenID Credential Issuer example",
+            "homepage_uri": "https://credential_issuer.example.org/home",
+            "policy_uri": "https://credential_issuer.example.org/policy",
+            "logo_uri": "https://credential_issuer.example.org/static/logo.svg",
+            "contacts": [
+                "tech@credential_issuer.example.org"
+            ]
+        }
+    },
+    "authority_hints": [
+        "https://intermediate.eidas.example.org"
+    ]
+}
+leaf_cred['jwks']['keys'] = [leaf_cred_jwk.serialize()]
+leaf_cred['metadata']['openid_credential_issuer']['jwks']['keys'] = [
+    leaf_cred_jwk_prot.serialize()]
+
+
+# Define intermediate Entity Statement for credential
+intermediate_es_cred = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": "https://intermediate.eidas.example.org",
+    "sub": "https://credential_issuer.example.org",
+    'jwks': {"keys": []}
+}
+intermediate_es_cred["jwks"]['keys'] = [leaf_cred_jwk.serialize()]
+
+# Define leaf Wallet Provider
+leaf_wallet_jwk = new_ec_key(ec_crv, alg=ec_alg)
+leaf_wallet = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": "https://wallet-provider.example.org",
+    "sub": "https://wallet-provider.example.org",
+    'jwks': {"keys": []},
+    "metadata": {
+        "wallet_provider": {
+            "jwks":  {"keys": []}
+        },
+        "federation_entity": {
+            "organization_name": "OpenID Wallet Verifier example",
+            "homepage_uri": "https://wallet-provider.example.org/home",
+            "policy_uri": "https://wallet-provider.example.org/policy",
+            "logo_uri": "https://wallet-provider.example.org/static/logo.svg",
+            "contacts": [
+                "tech@wallet-provider.example.org"
+            ]
+        }
+    },
+    "authority_hints": [
+        "https://intermediate.eidas.example.org"
+    ]
+}
+leaf_wallet['jwks']['keys'] = [leaf_wallet_jwk.serialize()]
+leaf_wallet['metadata']['wallet_provider'] = [leaf_wallet_jwk.serialize()]
+
+# Define intermediate Entity Statement for wallet provider
+intermediate_es_wallet = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": "https://intermediate.eidas.example.org",
+    "sub": "https://wallet-provider.example.org",
+    'jwks': {"keys": [leaf_wallet_jwk.serialize()]}
+}
+
+# Intermediate EC
+intermediate_ec = {
+    "exp": EXP,
+    "iat": NOW,
+    'iss': 'https://intermediate.eidas.example.org',
+    'sub': 'https://intermediate.eidas.example.org',
+    'jwks': {"keys": [intermediate_jwk.serialize()]},
+    'metadata': {
+        'federation_entity': {
+            'contacts': ['soggetto@intermediate.eidas.example.it'],
+            'federation_fetch_endpoint': 'https://intermediate.eidas.example.org/fetch',
+            'federation_resolve_endpoint': 'https://intermediate.eidas.example.org/resolve',
+            'federation_list_endpoint': 'https://intermediate.eidas.example.org/list',
+            'homepage_uri': 'https://soggetto.intermediate.eidas.example.it',
+            'name': 'Example Intermediate intermediate.eidas.example'
+        }
+    },
+    "authority_hints": [
+        "https://trust-anchor.example.org"
+    ]
+}
+
+
+# Define TA
+ta_es = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": "https://trust-anchor.example.org",
+    "sub": "https://intermediate.eidas.example.org",
+    'jwks': {"keys": [intermediate_jwk.serialize()]}
+}
+
+ta_ec = {
+    "exp": EXP,
+    "iat": NOW,
+    "iss": "https://trust-anchor.example.org",
+    "sub": "https://trust-anchor.example.org",
+    'jwks': {"keys": [ta_jwk.serialize()]},
+    "metadata": {
+        "federation_entity": {
+            'federation_fetch_endpoint': 'https://trust-anchor.example.org/fetch',
+            'federation_resolve_endpoint': 'https://trust-anchor.example.org/resolve',
+            'federation_list_endpoint': 'https://trust-anchor.example.org/list',
+            "organization_name": "TA example",
+            "homepage_uri": "https://trust-anchor.example.org/home",
+            "policy_uri": "https://trust-anchor.example.org/policy",
+            "logo_uri": "https://trust-anchor.example.org/static/logo.svg",
+            "contacts": [
+                "tech@trust-anchor.example.org"
+            ]
+        }
+    },
+    'constraints': {'max_path_length': 1}
+}
+
+# Sign step
+leaf_cred_signer = JWS(leaf_cred, alg=ec_alg,
+                       typ='entity-statement+jwt')
+leaf_cred_signed = leaf_cred_signer.sign_compact([leaf_cred_jwk])
+
+leaf_wallet_signer = JWS(leaf_wallet, alg=ec_alg,
+                         typ='entity-statement+jwt')
+leaf_wallet_signed = leaf_wallet_signer.sign_compact([leaf_wallet_jwk])
+
+
+intermediate_signer_ec = JWS(
+    intermediate_ec, alg=ec_alg,
+    typ="entity-statement+jwt"
+)
+intermediate_ec_signed = intermediate_signer_ec.sign_compact([
+                                                             intermediate_jwk])
+
+
+intermediate_signer_es_cred = JWS(
+    intermediate_es_cred, alg=ec_alg, typ='entity-statement+jwt')
+intermediate_es_cred_signed = intermediate_signer_es_cred.sign_compact([
+                                                                       intermediate_jwk])
+
+intermediate_signer_es_wallet = JWS(
+    intermediate_es_wallet, alg=ec_alg, typ='entity-statement+jwt')
+intermediate_es_wallet_signed = intermediate_signer_es_wallet.sign_compact([
+                                                                           intermediate_jwk])
+
+ta_es_signer = JWS(ta_es, alg=ec_alg, typ="entity-statement+jwt")
+ta_es_signed = ta_es_signer.sign_compact([ta_jwk])
+
+ta_ec_signer = JWS(ta_ec, alg=ec_alg, typ="entity-statement+jwt")
+ta_ec_signed = ta_ec_signer.sign_compact([ta_jwk])
+
+
+trust_chain_issuer = [
+    leaf_cred_signed,
+    intermediate_es_cred_signed,
+    ta_es_signed,
+    ta_ec_signed
+]
+
+trust_chain_wallet = [
+    leaf_wallet_signed,
+    intermediate_es_wallet_signed,
+    ta_es_signed
+]
+
+test_cred = tcv_test.StaticTrustChainValidator(
+    trust_chain_issuer, [ta_jwk.serialize()], httpc_params=httpc_params
+)
+assert test_cred.is_valid
+
+test_wallet = tcv_test.StaticTrustChainValidator(
+    trust_chain_wallet, [ta_jwk.serialize()], httpc_params=httpc_params
+)
+assert test_wallet.is_valid
+
+print(json.dumps(trust_chain_issuer, indent=2))
