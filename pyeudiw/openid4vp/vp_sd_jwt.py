b8a9b27d1ab3445b97231c2e0317926ea425d8b9
PascalDR <akira.zeta@gmail.com>
2023-12-13 15:57:05 +0100

    docs: documented content of vp_sd_jwt.py

--- pyeudiw/openid4vp/vp_sd_jwt.py
+++ pyeudiw/openid4vp/vp_sd_jwt.py
@@ -9,7 +9,18 @@ from pyeudiw.jwk.exceptions import KidNotFoundError
 from .exceptions import InvalidVPToken
 
 class VpSdJwt:
+    """Class for SD-JWT Format"""
+
     def __init__(self, jwt: str):
+        """
+        Generates a VpSdJwt istance
+
+        :param jwt: a string that represents the jwt.
+        :type jwt: str
+
+        :raises InvalidVPToken: if the jwt field's value is not a JWT.
+        """
+
         if not is_jwt_format(jwt):
             raise InvalidVPToken(f"VP is not in JWT format.")
 
@@ -24,7 +35,18 @@ class VpSdJwt:
         self,
         issuer_jwks_by_kid: Dict[str, dict] = {}
     ) -> bool:
+        """
+        Verifies a SDJWT.
 
+        :param jwks_by_kids: a dictionary that contains one or more JWKs with the KID as the key.
+        :type jwks_by_kids: Dict[str, dict]
+
+        :raises KidNotFoundError: if the needed kid is not inside the issuer_jwks_by_kid.
+        :raises NotImplementedError: the key_type of one or more JWK is not implemented.
+        :raises JWSVerificationError: if self.jwt field is not in a JWS Format.        
+
+        :returns: True if is valid, False otherwise.
+        """
         if not issuer_jwks_by_kid.get(self.credential_headers["kid"], None):
             raise KidNotFoundError(
                 f"issuer jwks {issuer_jwks_by_kid} doesn't contain "
