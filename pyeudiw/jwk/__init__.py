0247bc62c4cec2bab5f6945c54ceaf4d8765f475
PascalDR <akira.zeta@gmail.com>
2023-12-20 12:16:36 +0100

    feat: added specialized classes for JWK

--- pyeudiw/jwk/__init__.py
+++ pyeudiw/jwk/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 from typing import Union
 
@@ -6,14 +8,14 @@ from cryptojwt.jwk.ec import new_ec_key
 from cryptojwt.jwk.jwk import key_from_jwk_dict
 from cryptojwt.jwk.rsa import new_rsa_key
 
-from .exceptions import InvalidKid, KidNotFoundError
+from .exceptions import InvalidKid, KidNotFoundError, InvalidJwk
 
 KEY_TYPES_FUNC = dict(
     EC=new_ec_key,
     RSA=new_rsa_key
 )
 
-class JWK():
+class JWK:
     """
     The class representing a JWK istance
     """
@@ -117,6 +119,34 @@ class JWK():
     def __repr__(self):
         # private part!
         return self.as_json()
+    
+class RSAJWK(JWK):
+    def __init__(self, key: dict | None = None, hash_func: str = "SHA-256") -> None:
+        super().__init__(key, "RSA", hash_func, None)
+
+class ECJWK(JWK):
+    def __init__(self, key: dict | None = None, hash_func: str = "SHA-256", ec_crv: str = "P-256") -> None:
+        super().__init__(key, "EC", hash_func, ec_crv)
+
+def jwk_form_dict(key: dict, hash_func: str = "SHA-256") -> RSAJWK | ECJWK:
+    """
+    Returns a JWK instance from a dict.
+
+    :param key: a dict that represents the key.
+    :type key: dict
+
+    :returns: a JWK instance.
+    :rtype: JWK
+    """
+    _kty = key.get('kty', None)
+
+    if _kty == None or _kty not in ['EC', 'RSA']:
+        raise InvalidJwk("Invalid JWK")
+    elif _kty == "RSA":
+        return RSAJWK(key, hash_func)
+    else:
+        ec_crv = key.get('crv', "P-256")
+        return ECJWK(key, hash_func, ec_crv)
 
 def find_jwk(kid: str, jwks: list[dict], as_dict: bool=True) -> dict | JWK:
     """
