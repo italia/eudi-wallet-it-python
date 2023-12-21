f766608f6541892aa9014963bcd184aeb0797c6d
PascalDR <akira.zeta@gmail.com>
2023-12-21 15:08:07 +0100

    docs: documented content of mobile.py

--- pyeudiw/tools/mobile.py
+++ pyeudiw/tools/mobile.py
@@ -1,7 +1,16 @@
 from device_detector import DeviceDetector
 
 
-def is_smartphone(useragent: str):
+def is_smartphone(useragent: str) -> bool:
+    """Check if the useragent is a smartphone
+    
+    :param useragent: The useragent to check
+    :type useragent: str
+    :return: True if the useragent is a smartphone else False
+    :rtype: bool
+    """
+
     device = DeviceDetector(useragent).parse()
     if device.device_type() == 'smartphone':
         return True
+    return False
