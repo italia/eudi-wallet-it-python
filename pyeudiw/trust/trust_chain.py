f7faa9e4d7db45b884f930e177c4018a4907c7fd
PascalDR <akira.zeta@gmail.com>
2023-12-21 15:10:12 +0100

    docs: documented content of trust_chain.py

--- pyeudiw/trust/trust_chain.py
+++ pyeudiw/trust/trust_chain.py
@@ -1,28 +1,28 @@
 import logging
-from typing import List
 from typing import Optional
 
-from cryptojwt import KeyJar
 from cryptojwt.jwt import utc_time_sans_frac
-from pyeudiw.jwt.utils import decode_jwt_payload
+from pyeudiw.tools.base_logger import BaseLogger
 
 __author__ = "Roland Hedberg"
 __license__ = "Apache 2.0"
 __version__ = ""
 
-logger = logging.getLogger(__name__)
-
-class TrustChain(object):
+class TrustChain(BaseLogger):
     """
     Class in which to store the parsed result from applying metadata policies on a
     metadata statement.
     """
-
     def __init__(self,
                  exp: int = 0,
-                 verified_chain: Optional[list] = None):
+                 verified_chain: Optional[list] = None) -> None:
         """
+        Create a TrustChain instance.
+
         :param exp: Expiration time
+        :type exp: int
+        :param verified_chain: The verified chain
+        :type verified_chain: list
         """
         self.anchor = ""
         self.iss_path = []
@@ -32,10 +32,22 @@ class TrustChain(object):
         self.verified_chain = verified_chain
         self.combined_policy = {}
     
-    def keys(self):
+    def keys(self) -> list[str]:
+        """
+        Returns the metadata fields keys
+
+        :return: The metadata fields keys
+        :rtype: list
+        """
         return self.metadata.keys()
 
-    def items(self):
+    def items(self) -> list[tuple[str, dict]]:
+        """
+        Returns the metadata fields items
+
+        :return: The metadata fields items
+        :rtype: list[tuple[str, dict]]
+        """
         return self.metadata.items()
 
     def __getitem__(self, item):
@@ -50,23 +62,42 @@ class TrustChain(object):
         """
         return self.metadata
 
-    def is_expired(self):
+    def is_expired(self) -> bool:
+        """
+        Check if the trust chain is expired.
+
+        :return: True if the trust chain is expired else False
+        :rtype: bool
+        """
         now = utc_time_sans_frac()
         if self.exp < now:
-            logger.debug(f'is_expired: {self.exp} < {now}')
+            self._log_debug(
+                "Trust chain",
+                f'is_expired: {self.exp} < {now}'
+            )
             return True
         else:
             return False
 
-    def export_chain(self):
+    def export_chain(self) -> list:
         """
         Exports the verified chain in such a way that it can be used as value on the
         trust_chain claim in an authorization or explicit registration request.
-        :return:
+        
+        :return: The exported chain in reverse order
+        :rtype: list
         """
         _chain = self.verified_chain
         _chain.reverse()
         return _chain
     
     def set_combined_policy(self, entity_type: str, combined_policy: dict):
+        """
+        Set the combined policy for the given entity type.
+
+        :param entity_type: The entity type
+        :type entity_type: str
+        :param combined_policy: The combined policy
+        :type combined_policy: dict
+        """
         self.combined_policy[entity_type] = combined_policy
