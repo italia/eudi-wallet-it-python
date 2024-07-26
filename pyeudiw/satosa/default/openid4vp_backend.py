cd5abc9c5037f616d79960a75c9bdd595ffcb730
thomaschiozzi-tndigit <thomas.chiozzi@tndigit.it>
2024-07-26 14:37:14 +0200

    fix: added qr code info to ninja te

--- pyeudiw/satosa/default/openid4vp_backend.py
+++ pyeudiw/satosa/default/openid4vp_backend.py
@@ -216,6 +216,8 @@ class OpenID4VPBackend(OpenID4VPBackendInterface, BackendTrust):
             {
                 "qrcode_color": self.config["qrcode"]["color"],
                 "qrcode_text": res_url,
+                "qrcode_size": self.config["qrcode"]["sze"],
+                "qrcode_logo_path": self.config["qrcode"]["logo_path"],
                 "qrcode_expiration_time": self.config["qrcode"]["expiration_time"],
                 "state": state,
                 "status_endpoint": self.absolute_status_url
