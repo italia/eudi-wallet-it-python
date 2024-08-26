bd561883a90b9c04fd36366875e1c1f6f661c680
elisanp <elisa.nicolussi@tndigit.it>
2024-08-26 11:25:48 +0200

    fix: config validation schema

--- pyeudiw/satosa/schemas/endpoint.py
+++ pyeudiw/satosa/schemas/endpoint.py
@@ -1,16 +1,11 @@
-from pydantic import BaseModel, field_validator
+from typing import Union
+from pydantic import BaseModel
 
 
 class EndpointsConfig(BaseModel):
     pre_request: str
-    response: str
-    request: str
+    response: Union[str, dict]
+    request: Union[str, dict]
     entity_configuration: str
     status: str
     get_response: str
-
-    @field_validator('pre_request', 'response', 'request', 'entity_configuration', 'status', 'get_response')
-    def must_start_with_slash(cls, v):
-        if not v.startswith('/'):
-            raise ValueError(f'{v} must start with "/"')
-        return v
