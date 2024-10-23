655b13e4f186389e0b0eb7bab7a5a096aa604f21
PascalDR <akira.zeta@gmail.com>
2024-10-23 18:22:18 +0200

    feat: added as_public_dict

--- pyeudiw/jwk/__init__.py
+++ pyeudiw/jwk/__init__.py
@@ -116,6 +116,15 @@ class JWK:
         :rtype: dict
         """
         return self.jwk
+    
+    def as_public_dict(self) -> dict:
+        """
+        Returns the public key in format of dict.
+
+        :returns: The public key in form of dict.
+        :rtype: dict
+        """
+        return self.public_key
 
     def __repr__(self):
         # private part!
