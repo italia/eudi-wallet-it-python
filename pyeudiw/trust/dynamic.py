643bc8d12567e2be59f4ef3ea49f4084ae4aaed5
PascalDR <akira.zeta@gmail.com>
2024-10-24 18:37:49 +0200

    feat: refactoring of CombinedTrustEvaluator

--- pyeudiw/trust/dynamic.py
+++ pyeudiw/trust/dynamic.py
@@ -6,15 +6,15 @@ if float(f"{sys.version_info.major}.{sys.version_info.minor}") >= 3.12:
 else:
     from typing_extensions import TypedDict
 
-from pyeudiw.storage.base_storage import TrustType
 from pyeudiw.storage.db_engine import DBEngine
 from pyeudiw.tools.base_logger import BaseLogger
-from pyeudiw.tools.utils import get_dynamic_class, satisfy_interface
 from pyeudiw.trust.default import default_trust_evaluator
 from pyeudiw.trust.exceptions import TrustConfigurationError
 from pyeudiw.trust.interface import TrustEvaluator
 from pyeudiw.trust._log import _package_logger
-from pyeudiw.tools.utils import dynamic_class_loader
+from pyeudiw.tools.utils import dynamic_class_loader, satisfy_interface
+from pyeudiw.trust.handler.interface import TrustHandlerInterface
+from pyeudiw.trust.model.trust_source import TrustSourceData
 
 
 TrustModuleConfiguration_T = TypedDict("_DynamicTrustConfiguration", {"module": str, "class": str, "config": dict})
@@ -51,17 +51,36 @@ def dynamic_trust_evaluators_loader(trust_config: dict[str, TrustModuleConfigura
 
 
 class CombinedTrustEvaluator(TrustEvaluator, BaseLogger):
-    """
-    CombinedTrustEvaluator is a wrapper around multiple implementations of
-    TrustEvaluator. It's primary purpose is to handle how multiple configured
-    trust sources are queried when some metadata or key material is requested.
-    """
+    def __init__(
+            self, 
+            db_engine: DBEngine,
+            extracors: list[TrustHandlerInterface]
+        ) -> None:
+        self.db_engine: DBEngine = db_engine
+        self.extractors: list[TrustHandlerInterface] = extracors
+        self.extractors_names: list[str] = [e.name() for e in self.extractors]
+    
+    def _retrieve_trust_source(self, issuer: str) -> Optional[TrustSourceData]:
+        trust_source = self.db_engine.get_trust_source(issuer)
+        if trust_source:
+            return TrustSourceData.from_dict(trust_source)
+        return  None
+    
+    def _extract_trust_source(self, issuer: str) -> Optional[TrustSourceData]:
+        trust_source = TrustSourceData.empty()
 
-    def __init__(self, trust_evaluators: dict[str, TrustEvaluator], client_id: str):
-        self.trust_evaluators: dict[str, TrustEvaluator] = trust_evaluators
+        for extractor in self.extractors:
+            trust_source: TrustSourceData = extractor.extract(issuer, trust_source)
+        
+        self.db_engine.add_trust_source(issuer, trust_source.serialize())
 
-    def _get_trust_identifier_names(self) -> str:
-        return f'[{",".join(self.trust_evaluators.keys())}]'
+        return trust_source
+    
+    def _get_trust_source(self, issuer: str) -> TrustSourceData:
+        trust_source = self._retrieve_trust_source(issuer)
+        if not trust_source:
+            trust_source = self._extract_trust_source(issuer)
+        return trust_source
 
     def get_public_keys(self, issuer: str) -> list[dict]:
         """
@@ -70,38 +89,46 @@ class CombinedTrustEvaluator(TrustEvaluator, BaseLogger):
         :returns: a list of jwk(s); note that those key are _not_ necessarely
             identified by a kid claim
         """
-        pks: list[dict] = []
-        for eval_identifier, eval_instance in self.trust_evaluators.items():
-            try:
-                new_pks = eval_instance.get_public_keys(issuer)
-            except Exception as e:
-                self._log_warning(f"failed to find any key of issuer {issuer} with model {eval_identifier}: {eval_instance.__class__.__name__}", e)
-                continue
-            if new_pks:
-                pks.extend(new_pks)
-        if not pks:
-            raise Exception(f"no trust evaluator can provide cyptographic material for {issuer}: searched among: {self._get_trust_identifier_names()}")
-        return pks
+        trust_source = self._get_trust_source(issuer)
+
+        if not trust_source.keys:
+            raise Exception(f"no trust evaluator can provide cyptographic material for {issuer}: searched among: {self.extractors_names}")
+
+        return trust_source.public_keys
 
     def get_metadata(self, issuer: str) -> dict:
         """
         yields a dictionary of metadata about an issuer, according to some
         trust model.
         """
-        md: dict = {}
-        for instance in self.trust_evaluators.values():
-            md = instance.get_metadata(issuer)
-            if md:
-                return md
-        if not md:
-            raise Exception(f"no trust evaluator can provide metadata for {issuer}: searched among: {self._get_trust_identifier_names()}")
+        trust_source = self._get_trust_source(issuer)
+
+        if not trust_source.metadata:
+            raise Exception(f"no trust evaluator can provide metadata for {issuer}: searched among: {self.extractors_names}")
+
+        return trust_source.metadata
 
     def is_revoked(self, issuer: str) -> bool:
         """
         yield if the trust toward the issuer was revoked according to some trust model;
         this asusmed that  the isser exists, is valid, but is not trusted.
         """
-        raise NotImplementedError("implementation details yet to be deifined for combined use")
+        trust_source = self._get_trust_source(issuer)
+        return trust_source.is_revoked
 
     def get_policies(self, issuer: str) -> dict:
-        raise NotImplementedError("reserved for future uses")
+        trust_source = self._get_trust_source(issuer)
+
+        if not trust_source.policies:
+            raise Exception(f"no trust evaluator can provide policies for {issuer}: searched among: {self.extractors_names}")
+        
+        return trust_source.policies
+    
+    def get_selfissued_jwt_header_trust_parameters(self, issuer: str) -> dict:
+        trust_source = self._get_trust_source(issuer)
+
+        if not trust_source.trust_params:
+            raise Exception(f"no trust evaluator can provide trust parameters for {issuer}: searched among: {self.extractors_names}")
+        
+        return trust_source.trust_params
+        
