1ea832ab89447d078aaba459c68343332f534584
Giuseppe De Marco <demarcog83@gmail.com>
2024-11-19 15:26:58 +0100

    Apply suggestions from code review

--- pyeudiw/federation/trust_chain/parse.py
+++ pyeudiw/federation/trust_chain/parse.py
@@ -1,9 +1,7 @@
 from cryptojwt.jwk.ec import ECKey
 from cryptojwt.jwk.rsa import RSAKey
-from cryptojwt.jwk.okp import OKPKey
-from cryptojwt.jwk.hmac import SYMKey
 
 
 
-def get_public_key_from_trust_chain(trust_chain: list[str]) ->  ECKey | RSAKey | OKPKey | SYMKey | dict:
+def get_public_key_from_trust_chain(trust_chain: list[str]) ->  ECKey | RSAKey | dict:
     raise NotImplementedError("TODO")
