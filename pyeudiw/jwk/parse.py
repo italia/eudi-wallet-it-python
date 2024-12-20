9c36a374d3aa1e269ffdeeb43579a0dcbdb46259
Zicchio <chiozzi.thomas@gmail.com>
2024-12-20 17:26:06 +0100

    feat: unit test

--- pyeudiw/jwk/parse.py
+++ pyeudiw/jwk/parse.py
@@ -21,8 +21,9 @@ def adapt_key_to_JWK(key: dict | JWK | cryptojwt.jwk.JWK) -> JWK:
 def parse_key_from_x5c(x5c: list[str]) -> JWK:
     """Parse a key from an x509 chain. This function currently
     support only the parsing of public RSA key from such a chain.
+    The first element of the chain will contain the verifying key.
+    See RFC7517 https://datatracker.ietf.org/doc/html/rfc7517#section-4.7
     """
-    # TODO: unit test this function (it is important)
-    public_key = import_rsa_key(x5c)
+    public_key = import_rsa_key(x5c[0])
     key_dict = RSAKey(pub_key=public_key).to_dict()
     return JWK(key_dict)
