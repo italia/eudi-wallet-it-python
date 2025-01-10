cfaec490e1c1353244e14e974012afc665c79b36
Zicchio <chiozzi.thomas@gmail.com>
2025-01-10 15:42:28 +0100

    fix: update auth schema

--- pyeudiw/satosa/schemas/autorization.py
+++ pyeudiw/satosa/schemas/autorization.py
@@ -5,4 +5,5 @@ class AuthorizationConfig(BaseModel):
     url_scheme: str
     scopes: list[str]
     default_acr_value: HttpUrl
+    aud: str
     expiration_time: int = Field(..., gt=0)  # expiration_time must be greater than 0
