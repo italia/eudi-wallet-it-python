57039542f6ab3e8210ff8db19282e0d177cac722
Zicchio <33022304+Zicchio@users.noreply.github.com>
2025-01-21 17:58:56 +0100

    feat: support for direct_post (unencrypted) responses (#330)
    * wip: support unencrypted authz response
    
    * wip: support unencrypted authz response
    
    * feat: support unencrypted authorize response
    
    * fix: bug
    
    * fix: recovered old code
    
    * chore: cleanup, doc
    
    * fix: circular import
    
    ---------
    
    Co-authored-by: elisanp <118907120+elisanp@users.noreply.github.com>

--- pyeudiw/openid4vp/authorization_response.py
+++ pyeudiw/openid4vp/authorization_response.py
@@ -1,26 +1,110 @@
 from dataclasses import dataclass
 import json
+import satosa.context
 
 from pyeudiw.jwk.exceptions import KidNotFoundError
+from pyeudiw.jwt.exceptions import JWEDecryptionError
 from pyeudiw.jwt.jwe_helper import JWEHelper
 from pyeudiw.jwt.jws_helper import JWSHelper
-from pyeudiw.jwt.utils import decode_jwt_header, is_jwe_format, is_jwt_format
-
+from pyeudiw.jwt.utils import decode_jwt_header
 
 from cryptojwt.jwk.ec import ECKey
 from cryptojwt.jwk.rsa import RSAKey
+import cryptojwt.jwe.exception
 
-_RESPONSE_KEY = "response"
+from pyeudiw.openid4vp.exceptions import AuthRespParsingException, AuthRespValidationException
+from pyeudiw.openid4vp.interface import AuthorizationResponseParser
+from pyeudiw.openid4vp.schemas.response import AuthorizeResponseDirectPostJwt, AuthorizeResponsePayload, ResponseMode
 
 
-@dataclass
-class AuthorizeResponsePayload:
-    state: str
-    vp_token: str | list[str]
-    presentation_submission: dict
+def detect_response_mode(context: satosa.context.Context) -> ResponseMode:
+    """
+    Try to make inference on which response mode type this is based on the
+    content of an http request body
+    """
+    if "response" in context.request:
+        return ResponseMode.direct_post_jwt
+    if "vp_token" in context.request:
+        return ResponseMode.direct_post
+    raise AuthRespParsingException("HTTP POST request body does not contain a recognized openid4vp response mode")
 
-    def serialize_json(self) -> str:
-        return json.dumps(self.__dict__)
+
+def _check_http_post_headers(context: satosa.context.Context) -> None:
+    """
+    :raises AuthRespParsingException: if the request in the context does not \
+        look like a POST request
+    """
+    if (http_method := context.request_method.upper()) != "POST":
+        raise AuthRespParsingException(f"HTTP method [{http_method}] not supported")
+
+    # missing header is ok; but if it's there, it must be correct
+    if context.http_headers:
+        if (content_type := context.http_headers['HTTP_CONTENT_TYPE']) != "application/x-www-form-urlencoded":
+            raise AuthRespParsingException(f"HTTP content type [{content_type}] not supported")
+
+
+class DirectPostParser(AuthorizationResponseParser):
+    """DirectPostParser parses authorization responses sent as body of an
+    http post request.
+
+    The reference specification is defined here
+        https://openid.net/specs/openid-4-verifiable-presentations-1_0.html#name-response-parameters
+    """
+
+    def __init__(self):
+        pass
+
+    def parse_and_validate(self, context: satosa.context.Context) -> AuthorizeResponsePayload:
+        _check_http_post_headers(context)
+
+        resp_data: dict = context.request
+        try:
+            return AuthorizeResponsePayload(**resp_data)
+        except Exception as e:
+            raise AuthRespParsingException("invalid data in direct_post request body", e)
+
+
+class DirectPostJwtJweParser(AuthorizationResponseParser):
+    """DirectPostJwtJweParser parses authorization responses sent as body of an
+    http post request. The parser expectes a response wrapped in a jwt; more
+    precisely the managed response is x-www-form-urlencoded in the form of
+    response=<jwt> where <jwt> is an **encrypted but not signed** response.
+    As such, the class required a jwe helper with the correct key able to
+    decrypt the jwe.
+
+    The reference specification is defined here
+        https://openid.net/specs/openid-4-verifiable-presentations-1_0.html#name-response-mode-direct_postjw
+    """
+
+    def __init__(self, jwe_decryptor: JWEHelper):
+        self.jwe_decryptor = jwe_decryptor
+        pass
+
+    def parse_and_validate(self, context: satosa.context.Context) -> AuthorizeResponsePayload:
+        _check_http_post_headers(context)
+        resp_data_raw: dict = context.request
+        try:
+            resp_data = AuthorizeResponseDirectPostJwt(**resp_data_raw)
+        except Exception as e:
+            raise AuthRespParsingException("invalid data in direct_post.jwt request body", e)
+        try:
+            payload = self.jwe_decryptor.decrypt(resp_data.response)
+        except JWEDecryptionError as e:
+            raise AuthRespParsingException("invalid data in direct_post.jwt request body: not a jwe", e)
+        except cryptojwt.jwe.exception.DecryptionFailed:
+            raise AuthRespValidationException("invalid data in direct_post.jwt: unable to decrypt token")
+        except Exception as e:
+            # unfortunately library cryptojwt is not very exhaustive on why an operation failed...
+            raise AuthRespValidationException("invalid data in direct_post.jwt request body", e)
+
+        # iss, exp and aud MUST be OMITTED in the JWT Claims Set of the JWE
+        if ("iss" in payload) or ("exp" in payload):
+            raise AuthRespParsingException("response token contains an unexpected lifetime claims", Exception("wallet mishbeahiour: JWe with bad claims"))
+
+        try:
+            return AuthorizeResponsePayload(**payload)
+        except Exception as e:
+            raise AuthRespParsingException("invalid data in the direct_post.jwt: token payload does not have the expected claims", e)
 
 
 def _get_jwk_kid_from_store(jwt: str, key_store: dict[str, dict]) -> dict:
@@ -44,26 +128,3 @@ def _verify_and_decode_jwt(jwt: str, verifying_jwk: dict[dict, ECKey | RSAKey | 
     raw_payload: str = verifier.verify(jwt)["msg"]
     payload: dict = json.loads(raw_payload)
     return payload
-
-
-@dataclass
-class AuthorizeResponseDirectPost:
-    response: str  # jwt
-
-    def __post_init__(self):
-        jwt = self.response
-        if not is_jwe_format(jwt) and not is_jwt_format(jwt):
-            raise ValueError(f"input {_RESPONSE_KEY}={jwt} is neither jwt not jwe format")
-
-    def decode_payload(self, key_store_by_kid: dict[str, dict]) -> AuthorizeResponsePayload:
-        jwt = self.response
-        jwk_dict = _get_jwk_kid_from_store(jwt, key_store_by_kid)
-
-        payload = {}
-        if is_jwe_format(jwt):
-            payload = _decrypt_jwe(jwt, jwk_dict)
-        elif is_jwt_format(jwt):
-            payload = _verify_and_decode_jwt(jwt, jwk_dict)
-        else:
-            raise ValueError(f"unexpected state: input jwt={jwt} is neither a jwt nor a jwe")
-        return AuthorizeResponsePayload(**payload)
--- pyeudiw/openid4vp/interface.py
+++ pyeudiw/openid4vp/interface.py
@@ -1,7 +1,51 @@
 from cryptojwt.jwk.ec import ECKey
 from cryptojwt.jwk.rsa import RSAKey
+import satosa.context
 
 from pyeudiw.jwt.parse import KeyIdentifier_T
+from pyeudiw.openid4vp.schemas.response import AuthorizeResponsePayload
+
+
+class AuthorizationResponseParser:
+    """
+    AuthorizationResponseParser is an interface intended to parse direct POST
+    http responses.
+
+    An authorization parser is meant to just parse and eventually validate the
+    "lower" applicaiton layer of the transmission, that is, it is used to
+    extract an authorization response from the HTTP layer.
+    It SHOULD NOT be used to validate the actual content of the response, that
+    is, it SHOULD NOT try to validate vp_tokens, presentation_submissions, etc.
+    This is a delicate task that that is best suited for a different, dedicated
+    object, method or interface.
+    """
+
+    def parse_and_validate(self, context: satosa.context.Context) -> AuthorizeResponsePayload:
+        """
+        Parse (and optionally validate) a satosa http request, wrapped in its own
+        context, in order to extract an auhtorization response.
+        The validation step might include verification tasks; for example if the data is
+        reepresented as a jwt, the validation should perform a check on the jwt validity.
+
+        The concrete implementation SHOULD NOT be used to validate the actual content
+        of the response, that is, it SHOULD NOT try to validate vp_tokens,
+        presentation_submissions, etc.
+        This is a delicate task that that is best suited for a different, dedicated
+        object, method or interface.
+
+        :param context: an http request wrapped in its own satosa context
+        :type context: satosa.context.Context
+
+        :raises pyeudiw.openid4vp.exceptions.AuthRespParsingException: raised \
+            when the http response is malformed.
+        :raises pyeudiw.openid4vp.exceptions.AuthRespValidationException: raised \
+            when the http response is syntactically correct, but not valid (for \
+            example, it might be an expired token).
+
+        :return: the plain openid4vp authorization response; DCQL is not supported yet
+        :rtype: AuthorizeResponsePayload
+        """
+        raise NotImplementedError
 
 
 class VpTokenParser:
