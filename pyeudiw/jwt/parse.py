c609995bf1e89fdbd279fcdfa2db8781fe1a5048
PascalDR <akira.zeta@gmail.com>
2025-02-17 17:06:37 +0100

    fix: removed unused function

--- pyeudiw/jwt/parse.py
+++ pyeudiw/jwt/parse.py
@@ -25,14 +25,6 @@ class DecodedJwt:
         return unsafe_parse_jws(jws)
 
 
-def _unsafe_decode_part(part: str) -> dict:
-    padding_needed = len(part) % 4
-    if padding_needed:
-        part += "=" * (4 - padding_needed)
-    decoded_bytes = base64.urlsafe_b64decode(part)
-    return json.loads(decoded_bytes.decode("utf-8"))
-
-
 def unsafe_parse_jws(token: str) -> DecodedJwt:
     """
     Parse a token into its components.
