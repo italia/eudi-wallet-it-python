688557f00db43acf933800dcff1a0fa55b4943fb
PascalDR <akira.zeta@gmail.com>
2025-02-19 10:45:50 +0100

    fix: removed unused file

--- pyeudiw/federation/trust_chain/parse.py
+++
@@ -1,6 +0,0 @@
-from cryptojwt.jwk.ec import ECKey
-from cryptojwt.jwk.rsa import RSAKey
-
-
-def get_public_key_from_trust_chain(trust_chain: list[str]) -> ECKey | RSAKey | dict:
-    raise NotImplementedError("TODO")
