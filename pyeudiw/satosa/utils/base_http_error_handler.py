7aaa3ffd2e83fe8983411d78af9754ccce333398
PascalDR <akira.zeta@gmail.com>
2025-03-05 12:56:06 +0100

    fix: misleading function arguments

--- pyeudiw/satosa/utils/base_http_error_handler.py
+++ pyeudiw/satosa/utils/base_http_error_handler.py
@@ -9,8 +9,8 @@ class BaseHTTPErrorHandler(BaseLogger):
     def _serialize_error(
         self,
         context: Context,
-        message: str,
-        troubleshoot: str,
+        error: str,
+        description: str,
         err: str,
         err_code: str,
         level: str,
@@ -20,10 +20,10 @@ class BaseHTTPErrorHandler(BaseLogger):
 
         :param context: the request context
         :type context: satosa.context.Context
-        :param message: the error message
-        :type message: str
-        :param troubleshoot: the troubleshoot message
-        :type troubleshoot: str
+        :param error: the error type
+        :type error: str
+        :param description: the error description
+        :type description: str
         :param err: more info about the error
         :type err: str
         :param err_code: the error code
@@ -35,25 +35,30 @@ class BaseHTTPErrorHandler(BaseLogger):
         :rtype: JsonResponse
         """
 
-        _msg = f"{message}:"
+        _error = f"{error}:"
         if err:
-            _msg += f" {err}."
-        self._log(context, level=level, message=f"{_msg} {troubleshoot}")
+            _error += f" {err}."
+        self._log(context, level=level, message=f"{_error} {description}")
 
         return JsonResponse(
-            {"error": message, "error_description": troubleshoot}, status=err_code
+            {"error": error, "error_description": description}, status=err_code
         )
 
-    def _handle_500(self, context: Context, msg: str, err: Exception) -> JsonResponse:
+    def _handle_500(
+            self, 
+            context: Context, 
+            description: str, 
+            exc: Exception
+        ) -> JsonResponse:
         """
         Handles a 500 error.
 
         :param context: the request context
         :type context: satosa.context.Context
-        :param msg: the error message
-        :type msg: str
-        :param err: the exception raised
-        :type err: Exception
+        :param description: the error description
+        :type description: str
+        :param exc: the exception raised
+        :type exc: Exception
 
         :return: a json response containing the error
         :rtype: JsonResponse
@@ -62,8 +67,8 @@ class BaseHTTPErrorHandler(BaseLogger):
         return self._serialize_error(
             context,
             "server_error",
-            f"{msg}",
-            f"{msg}. {err.__class__.__name__}: {err}",
+            f"{description}",
+            f"{description}. {exc.__class__.__name__}: {exc}",
             "500",
             "error",
         )
@@ -71,24 +76,24 @@ class BaseHTTPErrorHandler(BaseLogger):
     def _handle_40X(
         self,
         code_number: str,
-        message: str,
+        error: str,
         context: Context,
-        troubleshoot: str,
-        err: Exception,
+        description: str,
+        exc: Exception,
     ) -> JsonResponse:
         """
         Handles a 40X error.
 
         :param code_number: the code number
         :type code_number: str
-        :param message: the error message
-        :type message: str
+        :param error: the error type
+        :type error: str
         :param context: the request context
         :type context: satosa.context.Context
-        :param troubleshoot: the troubleshoot message
-        :type troubleshoot: str
-        :param err: the exception raised
-        :type err: Exception
+        :param description: the error description
+        :type description: str
+        :param exc: the exception raised
+        :type exc: Exception
 
         :return: a json response containing the error
         :rtype: JsonResponse
@@ -96,65 +101,71 @@ class BaseHTTPErrorHandler(BaseLogger):
 
         return self._serialize_error(
             context,
-            message,
-            troubleshoot,
-            f"{err.__class__.__name__}: {err}",
+            error,
+            description,
+            f"{exc.__class__.__name__}: {exc}",
             f"40{code_number}",
             "error",
         )
 
     def _handle_400(
-        self, context: Context, troubleshoot: str, err: Exception = EmptyHTTPError("")
+        self, 
+        context: Context, 
+        description: str, 
+        exc: Exception = EmptyHTTPError("")
     ) -> JsonResponse:
         """
         Handles a 400 error.
 
         :param context: the request context
         :type context: satosa.context.Context
-        :param troubleshoot: the troubleshoot message
-        :type troubleshoot: str
-        :param err: the exception raised
-        :type err: Exception
+        :param description: the error description
+        :type description: str
+        :param exc: the exception raised
+        :type exc: Exception
 
         :return: a json response containing the error
         :rtype: JsonResponse
         """
-        return self._handle_40X("0", "invalid_request", context, troubleshoot, err)
+        return self._handle_40X("0", "invalid_request", context, description, exc)
 
     def _handle_401(
-        self, context, troubleshoot: str, err: EmptyHTTPError = EmptyHTTPError("")
+        self, context, description: str, exc: Exception = EmptyHTTPError("")
     ):
         """
         Handles a 401 error.
 
         :param context: the request context
         :type context: satosa.context.Context
-        :param troubleshoot: the troubleshoot message
-        :type troubleshoot: str
-        :param err: the exception raised
-        :type err: Exception
+        :param description: the error description
+        :type description: str
+        :param exc: the exception raised
+        :type exc: Exception
 
         :return: a json response containing the error
         :rtype: JsonResponse
         """
 
-        return self._handle_40X("1", "invalid_client", context, troubleshoot, err)
+        return self._handle_40X("1", "invalid_client", context, description, exc)
 
     def _handle_403(
-        self, context, troubleshoot: str, err: EmptyHTTPError = EmptyHTTPError("")
+        self, 
+        context, 
+        description: str, 
+        exc: Exception = EmptyHTTPError("")
     ):
         """
         Handles a 403 error.
 
         :param context: the request context
         :type context: satosa.context.Context
-        :param troubleshoot: the troubleshoot message
-        :type troubleshoot: str
-        :param err: the exception raised
-        :type err: Exception
+        :param description: the error description
+        :type description: str
+        :param exc: the exception raised
+        :type exc: Exception
 
         :return: a json response containing the error
         :rtype: JsonResponse
         """
 
-        return self._handle_40X("3", "expired", context, troubleshoot, err)
+        return self._handle_40X("3", "expired", context, description, exc)
