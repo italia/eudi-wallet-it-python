e5f11a23aa6a2a56730e126fee73be91a7debe21
PascalDR <akira.zeta@gmail.com>
2025-03-07 17:05:00 +0100

    fix: vaious fix

--- pyeudiw/x509/verify.py
+++ pyeudiw/x509/verify.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from ssl import DER_cert_to_PEM_cert
+from ssl import DER_cert_to_PEM_cert, PEM_cert_to_DER_cert
 
 import pem
 from cryptography.x509 import load_der_x509_certificate
@@ -34,7 +34,6 @@ def _verify_x509_certificate_chain(pems: list[str]):
 
         store_ctx = crypto.X509StoreContext(store, x509_certs[-1])
         store_ctx.verify_certificate()
-
         return True
     except crypto.Error as e:
         _message = f"cert's chain result invalid for the following reason -> {e}"
@@ -86,29 +85,26 @@ def _check_datetime(exp: datetime | None):
     return True
 
 
-def verify_x509_attestation_chain(
-    x5c: list[bytes], exp: datetime | None = None
-) -> bool:
+def verify_x509_attestation_chain(x5c: list[bytes]) -> bool:
     """
     Verify the x509 attestation certificate chain.
 
     :param x5c: The x509 attestation certificate chain
     :type x5c: list[bytes]
-    :param exp: The x509 attestation certificate chain expiration date
-    :type exp: datetime.datetime | None
 
     :returns: True if the x509 attestation certificate chain is valid else False
     :rtype: bool
     """
+    exp = get_expiry_date_from_x5c(x5c)
 
     if not _check_chain_len(x5c) or not _check_datetime(exp):
         return False
-
+    
     pems = [DER_cert_to_PEM_cert(cert) for cert in x5c]
 
     return _verify_x509_certificate_chain(pems)
 
-def to_pems_list(pem: str) -> list[str]:
+def to_pems_list(cert: str) -> list[str]:
     """
     Convert the x509 certificate chain from PEM to multiple PEMs.
 
@@ -118,7 +114,7 @@ def to_pems_list(pem: str) -> list[str]:
     :returns: The x509 certificate chain in PEM format
     :rtype: list[str]
     """
-    return [str(cert) for cert in pem.parse(pem)]
+    return [str(cert) for cert in pem.parse(cert)]
 
 def get_expiry_date_from_x5c(x5c: list[bytes]) -> datetime:
     """
@@ -143,8 +139,7 @@ def verify_x509_anchor(pem_str: str) -> bool:
     :returns: True if the x509 anchor certificate is valid else False
     :rtype: bool
     """
-
-    cert_data = load_der_x509_certificate(pem_str)
+    cert_data = load_der_x509_certificate(PEM_cert_to_DER_cert(pem_str))
 
     if not _check_datetime(cert_data.not_valid_after):
         logging.error(LOG_ERROR.format("check datetime failed"))
