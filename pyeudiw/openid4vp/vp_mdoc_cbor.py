eb7d31378723bec447fb3db8abe8c06315aa3be0
PascalDR <akira.zeta@gmail.com>
2025-03-11 15:31:04 +0100

    fix: added methods for validation

--- pyeudiw/openid4vp/vp_mdoc_cbor.py
+++ pyeudiw/openid4vp/vp_mdoc_cbor.py
@@ -1,19 +1,35 @@
 from pymdoccbor.mdoc.verifier import MdocCbor
+from datetime import datetime
+from pyeudiw.openid4vp.exceptions import MdocCborValidationError
 
-from pyeudiw.openid4vp.vp import Vp
-
-
-class VpMDocCbor(Vp):
+class VpMDocCbor:
     def __init__(self, data: str) -> None:
         self.data = data
         self.mdoc = MdocCbor()
         self.parse_digital_credential()
 
+    def get_credentials(self) -> dict:
+        return self.mdoc.data_as_cbor_dict()["issuerSigned"]
+    
+    def get_doc_type(self) -> str:
+        return self.mdoc.data_as_cbor_dict()["docType"]
+    
+    def is_revoked(self) -> bool:
+        return False
+    
+    def is_expired(self) -> bool:
+        exp_date = datetime.fromisoformat(
+            self.mdoc.data_as_cbor_dict()["issuerSigned"]["issuerAuth"]["validityInfo"]["validUntil"]
+        )
+
+        return exp_date < datetime.now()
+    
+    def verify_signature(self) -> None:
+        if self.mdoc.verify() == False:
+            raise MdocCborValidationError("Signature is invalid")
+        
     def parse_digital_credential(self) -> None:
         self.mdoc.load(data=self.data)
-
-    def verify(self, **kwargs) -> bool:
-        return self.mdoc.verify()
 
     def _detect_vp_type(self) -> str:
         return "mdoc_cbor"
