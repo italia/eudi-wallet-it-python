27832e802d8596e156630ad250889abc5be47bae
PascalDR <akira.zeta@gmail.com>
2025-03-13 12:19:52 +0100

    fix: get chains from configuration

--- pyeudiw/trust/handler/x509.py
+++ pyeudiw/trust/handler/x509.py
@@ -2,8 +2,7 @@ import ssl
 import logging
 from pyeudiw.trust.handler.interface import TrustHandlerInterface
 from pyeudiw.trust.model.trust_source import TrustSourceData, TrustParameterData
-from pyeudiw.jwk.parse import parse_key_from_x5c
-from pyeudiw.x509.verify import verify_x509_anchor, to_pems_list, get_expiry_date_from_x5c
+from pyeudiw.x509.verify import verify_x509_attestation_chain, get_expiry_date_from_x5c
 
 logger = logging.getLogger(__name__)
 
@@ -11,35 +10,40 @@ class X509Hanlder(TrustHandlerInterface):
     """
     X509Handler is a trust handler implementation that extracts trust material from x509 certificates.
     """
-    def __init__(self, client_id, **kwargs):
+    def __init__(
+        self, 
+        client_id: str, 
+        relying_party_certificate_chains_by_ca: dict[str, list[bytes]],
+        private_keys: list[dict[str, str]],
+        **kwargs
+    ):
         self.client_id = client_id
+        self.relying_party_certificate_chains_by_ca = relying_party_certificate_chains_by_ca
+        self.private_keys = private_keys
 
     def extract_and_update_trust_materials(
         self, issuer: str, trust_source: TrustSourceData
     ) -> TrustSourceData:
-        pem = ssl.get_server_certificate((issuer, 443))
+        issuer = issuer if issuer != "__internal__" else self.client_id
+        chain = self.relying_party_certificate_chains_by_ca.get(issuer)
 
-        if not verify_x509_anchor(pem):
+        if not chain:
+            logger.error(f"Invalid x509 anchor certificate for issuer {issuer}")
+            return trust_source
+
+        if not verify_x509_attestation_chain(chain):
             logger.error(f"Invalid x509 anchor certificate for issuer {issuer}")
             return trust_source
         
-        pems = to_pems_list(pem)
-
-        try:
-            jwk = parse_key_from_x5c(pems)
-        except Exception as e:
-            logger.error(f"Failed to parse x509 certificate chain for issuer {issuer}: {e}")
-            return trust_source
-        
-        exp = get_expiry_date_from_x5c(pems)
+        exp = get_expiry_date_from_x5c(chain)
 
         trust_source.add_trust_param(
             "x509",
             TrustParameterData(
                 attribute_name="x5c",
-                x5c=pems,
+                x5c=chain,
                 expiration_date=exp,
-                jwks=[jwk.as_dict()],
+                jwks=self.private_keys,
                 trust_handler_name=self.name,
             )
         )
