d06655e4e3e5dace21705fdafc8f8dfdb8a35b3d
Giuseppe De Marco <demarcog83@gmail.com>
2025-03-18 11:26:11 +0100

    Apply suggestions from code review

--- pyeudiw/openid4vp/vp_mdoc_cbor.py
+++ pyeudiw/openid4vp/vp_mdoc_cbor.py
@@ -1,6 +1,10 @@
 from pymdoccbor.mdoc.verifier import MdocCbor
 from datetime import datetime
 from pyeudiw.openid4vp.exceptions import MdocCborValidationError
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class VpMDocCbor:
     def __init__(self, data: str) -> None:
@@ -15,9 +19,15 @@ class VpMDocCbor:
         return False
     
     def is_expired(self) -> bool:
-        exp_date = datetime.fromisoformat(
-            self.mdoc.data_as_cbor_dict()["issuerSigned"]["issuerAuth"]["validityInfo"]["validUntil"]
-        )
+        _val_until: str = ""
+        try:
+            _val_until = self.mdoc.data_as_cbor_dict()["issuerSigned"]["issuerAuth"]["validityInfo"].get("validUntil")
+        except KeyError as e:
+            logger.error(f'Unconsitent issuerSigned schema ["issuerSigned"]["issuerAuth"]["validityInfo"], {e}, in mdoc cbor: {self.mdoc.data_as_cbor_dict()}')
+        if _val_until:
+            exp_date = datetime.fromisoformat(_val_until)
+        else:
+            logger.warning(f"Missing issuerSigned velidUntil in mdoc cbor: {self.mdoc.data_as_cbor_dict()}")
 
         return exp_date < datetime.now()
     
