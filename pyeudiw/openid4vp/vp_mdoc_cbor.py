667b9f855d966562872985c7eb78e2a2ea08ea32
PascalDR <akira.zeta@gmail.com>
2025-03-21 12:22:47 +0100

    feat: implemented expiration check

--- pyeudiw/openid4vp/vp_mdoc_cbor.py
+++ pyeudiw/openid4vp/vp_mdoc_cbor.py
@@ -1,18 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from pymdoccbor.mdoc.verifier import MdocCbor
 from pyeudiw.openid4vp.exceptions import MdocCborValidationError
 from pyeudiw.openid4vp.presentation_submission.base_vp_parser import BaseVPParser
 
 class VpMDocCbor(BaseVPParser):
     def _is_expired(self, mdoc: MdocCbor) -> bool:
+        for document in mdoc.documents:
+            try:
+                if document.issuersigned.issuer_auth.payload_as_dict['validityInfo']['validUntil'] < datetime.now(timezone.utc):
+                    return True
+            except KeyError:
+                return True
         return False
-        #Todo: Implement this method
-
-        exp_date = datetime.fromisoformat(
-            mdoc.data_as_cbor_dict["issuerSigned"]["issuerAuth"]["validityInfo"]["validUntil"]
-        )
-
-        return exp_date < datetime.now()
     
     def _is_revoked(self) -> bool:
         # TODO - revocation check here, using status list
@@ -26,6 +25,9 @@ class VpMDocCbor(BaseVPParser):
         ) -> None:
         mdoc = MdocCbor()
         mdoc.loads(data=token)
+
+        # Materiale crittografico da validare con trust evaluator
+        # mdoc.issuersigned.issuer_auth.x509_certificates
 
         if mdoc.verify() == False:
             raise MdocCborValidationError("Signature is invalid")
