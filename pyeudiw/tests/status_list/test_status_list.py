80c37c22d2f4731ecdd75a59aeb2542ab4a7d830
PascalDR <akira.zeta@gmail.com>
2025-04-08 17:13:08 +0200

    tests: adapted tests

--- pyeudiw/tests/status_list/test_status_list.py
+++ pyeudiw/tests/status_list/test_status_list.py
@@ -1,13 +1,9 @@
-from pyeudiw.status_list.parse import StatusListHelper
-
+from pyeudiw.status_list.helper import StatusListTokenHelper
 
 def test_statusListHelper():
-    helper = StatusListHelper(
+    helper = StatusListTokenHelper(
         """
-        {
-            "bits": 1,
-            "lst": "eNrbuRgAAhcBXQ"
-        }
+        eyJhbGciOiJFUzI1NiIsImtpZCI6IjEyIiwidHlwIjoic3RhdHVzbGlzdCtqd3QifQ.eyJleHAiOjIyOTE3MjAxNzAsImlhdCI6MTY4NjkyMDE3MCwiaXNzIjoiaHR0cHM6Ly9leGFtcGxlLmNvbSIsInN0YXR1c19saXN0Ijp7ImJpdHMiOjEsImxzdCI6ImVOcmJ1UmdBQWhjQlhRIn0sInN1YiI6Imh0dHBzOi8vZXhhbXBsZS5jb20vc3RhdHVzbGlzdHMvMSIsInR0bCI6NDMyMDB9.2RSRdUce0QmRvsbJkt0Hr0Ny5c9Tim2yj43wMFU76xjv9TClW5-B65b9pZSraeoPv6OxTULb4dHiWK0O8oLi6g
         """
     )
 
