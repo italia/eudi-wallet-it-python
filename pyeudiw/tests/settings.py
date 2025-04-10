462085d72e988c621d968edb7cbc4f10dee5896f
PascalDR <akira.zeta@gmail.com>
2025-04-10 12:20:54 +0200

    fix: use EC instead of RSA

--- pyeudiw/tests/settings.py
+++ pyeudiw/tests/settings.py
@@ -8,7 +8,7 @@ from pyeudiw.tests.x509.test_x509 import gen_chain
 from ssl import DER_cert_to_PEM_cert
 
 from pyeudiw.tests.federation.base import ta_jwk
-from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import ec
 
 BASE_URL = "https://example.com"
 AUTHZ_PAGE = "example.com"
@@ -22,39 +22,26 @@ def base64url_to_int(val):
     return int.from_bytes(base64.urlsafe_b64decode(val + '=='), 'big')
 
 jwk = {
-    "kty": "RSA",
+    "kty": "EC",
+    "d": "i0HQiqDPXf-MqC776ztbgOCI9-eARhcUczqJ-7_httc",
     "use": "sig",
-    "alg": "RS256",
-    "kid": "m00NPAelNBnG_wK2R5EpI_k-GWCHEUySamQYubgFjCg",
-    "d": "nMsnqz0lPHNGBgUqyuJ5nXQ0jh-mzs6d2xOY_QhpkRW1kEbexRJDdVV3fqMxj_s0MiF8mn-s8ea3e8cbNDgIy000Wvx05y1rMkB6KaZX2ZL5jwU7i_xP6NlLh8itikqJz7kKQSILgibQFFQDcScpEk8gUKa6fmSJQVwTII6GoJCdiJflv-FI2OQ_TCBQEEVVLpeUiVSP0n3OMUKGBlbaHOQkArUpla_ke_mtdfIrl7uB74Rxrin68KtFHkGDGdJPs-PPO1yJ2paFZI9QR_ettZ22v45c-qIgmCjsEnITDMaO9724PU_umlWsWe36Y9RAAzofKsjKqvA1OIzU03ob9Q",
-    "n": "sP6jt1XwJE0JDKxy4B7r3Jdb8W6bSRoVunyjWMgl5IafqFwHsJlYgCAWPeTrAL-iyjdnWC1csHuTqWjdndDL-oqEarrqoDAycVkfFTUTD81_wVhWUzAwxhQHiT7PTUIsV7m9VGlfC_kdCpQl5CcK1yx2nQ1KbqWOV1_5WnMgnN_EpNmztkZDnJmKedVduOb2dKWwnLS3fcGvUxXc87DjAzC2vfgQSoQfXAZbwItyS6OinFiUnBxRvt9ZY2IapjI1-wwDKKeRrqPC-fV2oWTrMqoYAvIDnf9AjKHAbIw7q301-7-eaUMF1hVtAz1XeXvMp0wK8_uSo9Vgv1vHhBpOwQ",
-    "e": "AQAB",
-    "p": "0ViKTSyZdLtvbLBpTvVAXTdrhTwGXuh16PadQMAVmkoxOPiExRB5uLiy2ADaVKSglia5aQBUp9v0ygEEOmkiUtn5A26D9ui0dkPR0hx4fwqCOOmA2ZyDUNFJ_qrGSwT1SxGQDHeRteymJG7uN9QekS3XiBDgFJxwl-vVpoSTBJM",
-    "q": "2HBr9qhVd3zZUQuNb7ro06ErLl4fhL-DiKsNqXB772tDNTJYeog1nOWgS22tcv5WHrSoYF1x5Q74YVoA6yVj6DwFx2Hc2pYZazzhYMRC3NAWkTEdroy9IjtpzKIpQIqw-sq8CbWVBXzho8uQBCdg8h73z11_HPyXT9BqQCmxJ9s",
-    "dp": "WsQ32rQuqNUnv4lRb4GYcZI41SCsZnQFw4dBsTRXaXknlFr0PfkhvXyfVlYwU6i5U8DgfO0-xzTwErGUIrs4vZFyjRFauDA3JlvLWn0rpXFp-sELM87PhLfpjDiBFz_EFtM7kJw7GhTMCFnsgVpAEpQ8sesXLPiTPNts2_D5SW8",
-    "dq": "jWlucLrtFGOjDRuyLjT9l__uWZ4vk6kZRHsWMwWGRBhd0ezx-CT0em1hPMcNE1vvYqKAfG2xU4pjaB_JB9nnG73TvMBI7xwwwWsGihXQ5bqjc_uWPAxCKpKM_qFYuI2lMkaxctqL4gkE1-LRVpVv9uGa4YZh3ct_BSvTr9ZNpA8",
-    "qi": "kn9Etj4a2erCUmoZUQalPjHxCRYm5Q3wAkFIRGSQADA51mkwQHyTYqXbHcmXn2ZgXBVI6XDWJB51Me-NCPfITTlusqxvATF7Q-QJtdK_FbgNtcVRNc1FMq_M7VBHA1i9wJR7T4t57aywfXPmlsA5TToTDRe-ybdw0C3ys4KQATs"
+    "crv": "P-256",
+    "kid": "SQgNjv4yU8sfuafJ2DPWq2tnOlK1JSibd3V5KqYRhOk",
+    "x": "Q46FDkhMjewZIP9qP8ZKZIP-ZEemctvjxeP0l3vWHMI",
+    "y": "IT7lsGxdJewmonk9l1_TAVYx_nixydTtI1Sbn0LkfEA",
+    "alg": "ES256"
 }
 
-# Extract components from JWK
-_n = base64url_to_int(jwk['n'])
-_e = base64url_to_int(jwk['e'])
 _d = base64url_to_int(jwk['d'])
-_p = base64url_to_int(jwk['p'])
-_q = base64url_to_int(jwk['q'])
-_dp = base64url_to_int(jwk['dp'])
-_dq = base64url_to_int(jwk['dq'])
-_qi = base64url_to_int(jwk['qi'])
-
-# Create RSA private key
-private_key = rsa.RSAPrivateNumbers(
-    p=_p,
-    q=_q,
-    d=_d,
-    dmp1=_dp,
-    dmq1=_dq,
-    iqmp=_qi,
-    public_numbers=rsa.RSAPublicNumbers(e=_e, n=_n)
+_x = base64url_to_int(jwk['x'])
+_y = base64url_to_int(jwk['y'])
+private_key = ec.EllipticCurvePrivateNumbers(
+    private_value=_d,
+    public_numbers=ec.EllipticCurvePublicNumbers(
+        x=_x,
+        y=_y,
+        curve=ec.SECP256R1()
+    )
 ).private_key()
 
 DEFAULT_X509_CHAIN = gen_chain(
@@ -276,6 +263,7 @@ CONFIG = {
                 },
                 "default_sig_alg": "RS256",
                 "federation_jwks": [
+                    jwk,
                     {
                         "kty": "RSA",
                         "d": "QUZsh1NqvpueootsdSjFQz-BUvxwd3Qnzm5qNb-WeOsvt3rWMEv0Q8CZrla2tndHTJhwioo1U4NuQey7znijhZ177bUwPPxSW1r68dEnL2U74nKwwoYeeMdEXnUfZSPxzs7nY6b7v"
