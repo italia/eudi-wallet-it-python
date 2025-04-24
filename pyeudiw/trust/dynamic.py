5c4fed9b61b7ecc22c883b192f57d5ed279cfb9e
Zicchio <33022304+Zicchio@users.noreply.github.com>
2025-04-24 15:12:04 +0200

    fix: removed exception that should not be raised

--- pyeudiw/trust/dynamic.py
+++ pyeudiw/trust/dynamic.py
@@ -199,10 +199,7 @@ class CombinedTrustEvaluator(BaseLogger):
                         else:
                             used_handlers.append(handler.__class__.__name__)
 
-            raise NoCriptographicMaterial(
-                f"no trust evaluator can provide cyptographic material "
-                f"for {issuer}: searched among: {self.handlers_names}"
-            )
+            self._log_warning(f"no configured trust handler can successfully process static trust material {static_trust_materials} of issuer {issuer}")
 
         # try with handlers that don't use static trust materials like DirectTrustJar
         filetered_handlers = [handler for handler in self.handlers if handler.__class__.__name__ not in used_handlers]
