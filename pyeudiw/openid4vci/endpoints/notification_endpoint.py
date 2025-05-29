c0b3d207b7cbfe9f4b59163497e1ab6a6800ecd5
sara.longobardi <sara.longobardi@accenture.com>
2025-05-29 13:09:56 +0200

    rev: openid4vci, exposing notification endpoint configuration (view: 444#discussion_r2113343142)

---
+++ pyeudiw/openid4vci/endpoints/notification_endpoint.py
@@ -0,0 +1,61 @@
+from typing import Type
+
+from satosa.context import Context
+from satosa.response import (
+    Response
+)
+
+from pyeudiw.jwt.jws_helper import JWSHelper
+from pyeudiw.openid4vci.endpoints.base_endpoint import BaseEndpoint
+from pyeudiw.openid4vci.models.notification_request import NotificationRequest
+from pyeudiw.openid4vci.utils.response import (
+    NoContent
+)
+from pyeudiw.tools.content_type import (
+    HTTP_CONTENT_TYPE_HEADER,
+    APPLICATION_JSON
+)
+from pyeudiw.tools.exceptions import (
+    InvalidRequestException,
+    InvalidScopeException
+)
+from pyeudiw.tools.validation import (
+    validate_content_type,
+    validate_request_method
+)
+
+
+class NotificationHandler(BaseEndpoint):
+
+    def __init__(self, config: dict, base_url: str, name: str):
+        """
+        Initialize the notification endpoint class.
+        Args:
+            config (dict): The configuration dictionary.
+            base_url (str): The base URL of the service.
+            name (str): The name of the SATOSA module to append to the URL.
+        """
+        super().__init__(config, base_url, name)
+        self.jws_helper = JWSHelper(self.config["metadata_jwks"])
+
+    def notification_endpoint(self, context: Context) -> Type[NoContent] | Response:
+        """
+        Handle a POST request to the notification endpoint.
+        Args:
+            context (Context): The SATOSA context.
+        Returns:
+            A Response object.
+        """
+        try:
+            validate_request_method(context.request_method, ["POST"])
+            validate_content_type(context.http_headers[HTTP_CONTENT_TYPE_HEADER], APPLICATION_JSON)
+            NotificationRequest.model_validate(**context.request.body.decode("utf-8"))
+            return NoContent
+        except (InvalidRequestException, InvalidScopeException) as e:
+            return self._handle_400(context, e.message, e)
+        except Exception as e:
+            self._log_error(
+                e.__class__.__name__,
+                f"Error during invoke notification endpoint: {e}"
+            )
+            return self._handle_500(context, "error during invoke notification endpoint", e)
