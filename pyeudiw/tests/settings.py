bbee941d517053eaa813604f7d2351e7ee791009
PascalDR <akira.zeta@gmail.com>
2025-06-09 16:07:50 +0200

    fix: changed client_id

--- pyeudiw/tests/settings.py
+++ pyeudiw/tests/settings.py
@@ -340,7 +340,7 @@ CONFIG = {
             "module": "pyeudiw.trust.handler.x509",
             "class": "X509Handler",
             "config": {
-                "client_id": f"x509_san_dns:...",
+                "client_id": f"x509_san_dns:{BASE_URL}",
                 "include_issued_jwt_header_param": True,
                 "relying_party_certificate_chains_by_ca": {
                     "ca.example.com": DEFAULT_X509_CHAIN,
