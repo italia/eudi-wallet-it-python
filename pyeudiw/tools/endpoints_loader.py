756e11901f4bce001118ff8e9fa8c400b4706169
PascalDR <akira.zeta@gmail.com>
2025-06-23 18:00:14 +0200

    fix: fail for a wrong configuration

--- pyeudiw/tools/endpoints_loader.py
+++ pyeudiw/tools/endpoints_loader.py
@@ -1,4 +1,10 @@
+from typing import Callable, Any
 from pyeudiw.tools.utils import get_dynamic_class
+from satosa.context import Context
+from satosa.internal import InternalData
+from satosa.response import Response
+from satosa.attribute_mapping import AttributeMapper
+
 
 
 class EndpointsLoader:
@@ -11,7 +17,9 @@ class EndpointsLoader:
             config: dict[str, dict[str, str] | list[str]],
             internal_attributes: dict[str, dict[str, str | list[str]]],
             base_url: str,
-            name: str
+            name: str,
+            auth_callback_func: Callable[[Context, InternalData], Response],
+            converter: AttributeMapper | None = None
     ):
         """
         Create a backend/frontend dynamically.
@@ -26,7 +34,14 @@ class EndpointsLoader:
         :rtype: object
         """
 
-        endpoints= config.get("endpoints", None)
+        endpoints: dict[str, Any] | None = config.get("endpoints", None)
+
+        if not endpoints:
+            raise ValueError("No endpoints configured in the OpenID4VCI config")
+        
+        if not isinstance(endpoints, dict):
+            raise ValueError("Endpoints configuration must be a dictionary")
+
         endpoint_instances = {}
         for e in endpoints.values():
             if (
@@ -35,7 +50,7 @@ class EndpointsLoader:
                     and e.get("class", None)
             ):
                 endpoint_class = get_dynamic_class(e["module"], e["class"])
-                endpoint_instances[e["path"].lstrip("/")] = endpoint_class(config, internal_attributes, base_url, name)
+                endpoint_instances[e["path"].lstrip("/")] = endpoint_class(config, internal_attributes, base_url, name, auth_callback_func, converter)
 
         self.endpoint_instances = endpoint_instances
 
