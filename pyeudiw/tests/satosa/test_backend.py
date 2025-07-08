2eb1b52e99bd8ac9d5986640bc42176653244052
PascalDR <akira.zeta@gmail.com>
2025-07-08 18:07:13 +0200

    fix: removed duplicated

--- pyeudiw/tests/satosa/test_backend.py
+++ pyeudiw/tests/satosa/test_backend.py
@@ -82,8 +82,6 @@ mdoci = MdocCborIssuer(
         "state_or_province_name": "California",
         "locality_name": "San Francisco",
         "organization_name": "Micov",
-        "common_name": "My Company",
-        "san_url": "mysite.com",
         "not_valid_before": datetime.now(timezone.utc) - timedelta(days=1),
         "not_valid_after": datetime.now(timezone.utc) + timedelta(days=10),
         "san_url": "https://credential-issuer.example.org"
