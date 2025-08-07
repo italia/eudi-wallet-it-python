0eba8da2769e0d2f64acdcfd8133bb040869a802
PascalDR <akira.zeta@gmail.com>
2025-08-07 17:50:23 +0200

    feat: initial implementation of handle_authn_response

--- pyeudiw/satosa/frontends/openid4vci/openid4vci.py
+++ pyeudiw/satosa/frontends/openid4vci/openid4vci.py
@@ -9,7 +9,10 @@ from satosa.frontends.base import FrontendModule
 from satosa.internal import InternalData
 from satosa.response import Response
 
+from pyeudiw.satosa.frontends.openid4vci.models.authorization_response import AuthorizationResponse
 from pyeudiw.satosa.frontends.openid4vci.storage.engine import OpenId4VciDBEngineHandler
+from pyeudiw.satosa.frontends.openid4vci.storage.entity import OpenId4VCIEntity
+from pyeudiw.satosa.frontends.openid4vci.tools.exceptions import InvalidRequestException
 from pyeudiw.satosa.utils.session import get_session_id
 from pyeudiw.tools.endpoints_loader import EndpointsLoader
 
@@ -56,6 +59,27 @@ class OpenID4VCIFrontend(FrontendModule):
     logger.debug(f"Loaded OpenID4VCI endpoints: {url_map}")
     return url_map
 
-  def handle_authn_response(self, context: Context, internal_resp: InternalData) -> None:
-    #TODO: never reached, needs to be integrated
-    self.db_engine.update_attributes_by_session_id(get_session_id(context), internal_resp.attributes)
+  def handle_authn_response(self, context: Context, internal_resp: InternalData):
+    try:
+      session_id = get_session_id(context)
+      entity = self.db_engine.search_session_by_field("session_id", session_id)
+
+      if not entity:
+        logger.error(f"Session with ID {session_id} not found in storage")
+        raise InvalidRequestException(f"Session with ID {session_id} not found in storage")
+
+      vci_entity = OpenId4VCIEntity(**entity)
+      vci_entity.attributes = internal_resp.attributes
+
+      self.db_engine.upsert_session(vci_entity.session_id, vci_entity.model_dump())
+
+      return AuthorizationResponse(
+          state=vci_entity.state,
+          iss=self.config.get("metadata", {}).get("openid_credential_issuer", {}).get("credential_issuer", ""),
+      ).to_redirect_response(vci_entity.redirect_uri)
+    except InvalidRequestException as e:
+      logger.error(f"Invalid request: {e}")
+      return Response(status='400', message=str(e))
+    except Exception as e:
+      logger.error(f"Error handling authentication response: {e}")
+      return Response(status='500', message="Internal Server Error")
