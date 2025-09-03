5ed837bd1f0407174530c446d77fcd6ab6c546d2
PascalDR <akira.zeta@gmail.com>
2025-09-03 16:22:53 +0200

    fix: better dpop proof handling

--- pyeudiw/oauth2/dpop/verifier.py
+++ pyeudiw/oauth2/dpop/verifier.py
@@ -2,12 +2,14 @@ import base64
 import hashlib
 import logging
 
+from typing import Optional
+
 from pyeudiw.jwk.exceptions import KidError
 from pyeudiw.jwk.schemas.public import JwkSchema
 from pyeudiw.jwt.jws_helper import JWSHelper
 
 from pyeudiw.jwt.utils import decode_jwt_header, decode_jwt_payload
-from pyeudiw.oauth2.dpop.exceptions import InvalidDPoP, InvalidDPoPAth, InvalidDPoPKid
+from pyeudiw.oauth2.dpop.exceptions import InvalidDPoP, InvalidDPoPKid
 from pyeudiw.oauth2.dpop.schema import DPoPTokenHeaderSchema, DPoPTokenPayloadSchema
 
 logger = logging.getLogger(__name__)
@@ -21,9 +23,8 @@ class DPoPVerifier:
 
     def __init__(
         self,
-        public_jwk: dict,
-        http_header_authz: str,
         http_header_dpop: str,
+        http_header_authz: Optional[str] = None,
     ):
         """
         Generate an instance of DPoPVerifier.
@@ -31,29 +32,26 @@ class DPoPVerifier:
         :param public_jwk: a dict representing the public JWK of DPoP.
         :type public_jwk: dict
         :param http_header_authz: a string representing the authz value.
-        :type http_header_authz: str
+        :type http_header_authz: Optional[str]
         :param http_header_dpop: a string representing the DPoP value.
         :type http_header_dpop: str
 
         :raises ValueError: if DPoP proof is not a valid JWT
 
         """
-        self.public_jwk = public_jwk
-        self.dpop_token = (
-            http_header_authz.replace(self.dpop_header_prefix, "")
-            if self.dpop_header_prefix in http_header_authz
-            else http_header_authz
-        )
-        # If the jwk is invalid, raise an exception
-        try:
-            JwkSchema(**public_jwk)
-        except Exception as e:
-            logger.error("Jwk validation error, " f"{e.__class__.__name__}: {e}")
-            raise ValueError("JWK schema validation error during DPoP init")
+
+        self.dpop_authz_token = None
+
+        if http_header_authz:
+            self.dpop_authz_token = (
+                http_header_authz.replace(self.dpop_header_prefix, "")
+                if self.dpop_header_prefix in http_header_authz
+                else http_header_authz
+            )
 
         # If the jwt is invalid, this will raise an exception
         try:
-            decode_jwt_header(http_header_dpop)
+            dpop_header = decode_jwt_header(http_header_dpop)
         except UnicodeDecodeError as e:
             logger.error("DPoP proof validation error, " f"{e.__class__.__name__}: {e}")
             raise ValueError("DPoP proof is not a valid JWT")
@@ -61,6 +59,15 @@ class DPoPVerifier:
             logger.error("DPoP proof validation error, " f"{e.__class__.__name__}: {e}")
             raise ValueError("DPoP proof is not a valid JWT")
         self.proof = http_header_dpop
+
+        # If the jwk is invalid, raise an exception
+        try:
+            JwkSchema(**dpop_header.get("jwk", {}))
+        except Exception as e:
+            logger.error("Jwk validation error, " f"{e.__class__.__name__}: {e}")
+            raise ValueError("JWK schema validation error during DPoP init")
+
+        self.public_jwk = dpop_header["jwk"]
 
     @property
     def is_valid(self) -> bool:
@@ -95,20 +102,14 @@ class DPoPVerifier:
         header = decode_jwt_header(self.proof)
         DPoPTokenHeaderSchema(**header)
 
-        if header["jwk"] != self.public_jwk:
-            raise InvalidDPoPAth(
-                (
-                    "DPoP proof validation error,  "
-                    "header['jwk'] != self.public_jwk, "
-                    f"{header['jwk']} != {self.public_jwk}"
-                )
-            )
-
         payload = decode_jwt_payload(self.proof)
         DPoPTokenPayloadSchema(**payload)
 
-        _ath = hashlib.sha256(self.dpop_token.encode())
-        _ath_b64 = base64.urlsafe_b64encode(_ath.digest()).rstrip(b"=").decode()
-        proof_valid = _ath_b64 == payload["ath"]
+        if self.dpop_authz_token:
+            _ath = hashlib.sha256(self.dpop_authz_token.encode())
+            _ath_b64 = base64.urlsafe_b64encode(_ath.digest()).rstrip(b"=").decode()
+            proof_valid = _ath_b64 == payload["ath"]
+
+            return dpop_valid and proof_valid
         
-        return dpop_valid and proof_valid
+        return dpop_valid
