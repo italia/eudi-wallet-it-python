fd709d7f1ce7aa5be55f64559f6d3a5574f3fa1f
sara.longobardi <sara.longobardi@accenture.com>
2025-09-17 19:35:42 +0200

    feat: upgrade python, add support from py3.12 to py3.15

--- setup.py
+++ setup.py
@@ -21,9 +21,10 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3.15",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     url='https://github.com/italia/eudi-wallet-it-python',
