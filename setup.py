851348bf4fd8d906d5eff786dfb447dcc77ea2f4
sara.longobardi <sara.longobardi@accenture.com>
2025-09-18 16:51:42 +0200

    feat: upgrade python, restore support to py.3.11

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Libraries :: Python Modules"
