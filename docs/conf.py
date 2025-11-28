865881c1a87874f89c99d037c183f14d66f152b0
manpace <221346462+manpace@users.noreply.github.com>
2025-11-28 12:32:15 +0100

    fixed docs workflow: - upgrade gh-pages python version to 3.12 - fix docs/config.py - added docs requirements in setup.py - cryptography minimum compatible version set

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ author = 'Developers Italia'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
@@ -31,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
