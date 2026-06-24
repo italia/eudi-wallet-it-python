8e0f72fc5aa5b9353583a9e0d10d4974f099bee2
peppelinux <demarcog83@gmail.com>
2026-06-24 07:52:48 +0200

    fix: federation jwks for signing EC

--- pyeudiw/tests/satosa/frontends/openid4vci/endpoints/test_metadata_endpoint.py
+++ pyeudiw/tests/satosa/frontends/openid4vci/endpoints/test_metadata_endpoint.py
@@ -4,6 +4,9 @@ from copy import deepcopy
 import pytest
 from satosa.context import Context
 
+from pyeudiw.jwk import JWK
+from pyeudiw.jwt.exceptions import JWSVerificationError
+from pyeudiw.jwt.jws_helper import JWSHelper
 from pyeudiw.jwt.utils import base64_urldecode
 from pyeudiw.satosa.frontends.openid4vci.endpoints.metadata_endpoint import (
     MetadataHandler,
@@ -16,6 +19,7 @@ from pyeudiw.tests.satosa.frontends.openid4vci.mock_openid4vci import (
     MOCK_CREDENTIAL_CONFIGURATIONS,
     MOCK_CREDENTIAL_STORAGE_CONFIG,
     MOCK_ENDPOINTS_CONFIG,
+    MOCK_FEDERATION_JWKS_CONFIG,
     MOCK_INTERNAL_ATTRIBUTES,
     MOCK_JWT_CONFIG,
     MOCK_METADATA_JWKS_CONFIG,
@@ -172,8 +176,19 @@ def test_endpoint_returns_jwt(metadata_handler, context):
     jwt_parts = response.message.split(".")
     header = json.loads(base64_urldecode(jwt_parts[0]))
     assert header["alg"] == "ES256"
-    assert header["kid"] == MOCK_PYEUDIW_FRONTEND_CONFIG["metadata_jwks"][0]["kid"]
+    assert header["kid"] == MOCK_FEDERATION_JWKS_CONFIG[0]["kid"]
     assert header["typ"] == "entity-statement+jwt"
+
+    federation_public_keys = [
+        JWK(k).as_public_dict() for k in MOCK_FEDERATION_JWKS_CONFIG
+    ]
+    JWSHelper(federation_public_keys).verify(response.message)
+
+    metadata_public_keys = [
+        JWK(k).as_public_dict() for k in MOCK_METADATA_JWKS_CONFIG
+    ]
+    with pytest.raises(JWSVerificationError):
+        JWSHelper(metadata_public_keys).verify(response.message)
 
     payload = json.loads(base64_urldecode(jwt_parts[1]))
     _assert_metadata(
